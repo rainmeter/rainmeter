0a5d07eef2dfe8263a4b37d332083cbdd2d1b50d
Birunthan Mohanathas <birunthan@mohanathas.com>
2010-11-13 20:16:04 +0000

    Small change to how PRODUCTVERSION is determined (thanks spx!).

--- revision-number.h
+++ revision-number.h
@@ -1,8 +1,8 @@
 #pragma once
-#define FILEVER        1,4,0,593
-#define PRODUCTVER     1,4,0,593
-#define STRFILEVER     "1.4.0.593"
-#define STRPRODUCTVER  "1.4.0.593 (64-bit)"
+#define FILEVER        1,4,0,601
+#define PRODUCTVER     1,4,0,601
+#define STRFILEVER     "1.4.0.601"
+#define STRPRODUCTVER  "1.4.0.601"
 
-const int revision_number = 593;
+const int revision_number = 601;
 const bool revision_beta = true;
