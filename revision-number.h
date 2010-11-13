fa351991a51456ae633612ec9df286c5e5b5d81d
Birunthan Mohanathas <birunthan@mohanathas.com>
2010-11-13 12:51:20 +0000

    Replaced UpdateApplicationRC.exe and UpdatePluginRC.exe with a more efficient method.

--- revision-number.h
+++ revision-number.h
@@ -1,3 +1,8 @@
 #pragma once
-const int revision_number = 592;
+#define FILEVER        1,4,0,593
+#define PRODUCTVER     1,4,0,593
+#define STRFILEVER     "1.4.0.593"
+#define STRPRODUCTVER  "1.4.0.593 (64-bit)"
+
+const int revision_number = 593;
 const bool revision_beta = true;
