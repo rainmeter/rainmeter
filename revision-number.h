d77709a50ec14ed57ad9baefe802fb482d70baee
jsmorley <jsmorley@gmail.com>
2010-12-05 00:52:07 +0000

    Adding InputTextPlugin to the .svn and changing the build to include the pre-compiled version. This is a new plugin written by psouza4 in C#. Further details on how to compile locally will be forthcoming.

--- revision-number.h
+++ revision-number.h
@@ -1,8 +1,8 @@
 #pragma once
-#define FILEVER        1,4,0,601
-#define PRODUCTVER     1,4,0,601
-#define STRFILEVER     "1.4.0.601"
-#define STRPRODUCTVER  "1.4.0.601"
+#define FILEVER        1,4,0,638
+#define PRODUCTVER     1,4,0,638
+#define STRFILEVER     "1.4.0.638"
+#define STRPRODUCTVER  "1.4.0.638"
 
-const int revision_number = 601;
+const int revision_number = 638;
 const bool revision_beta = true;
