dc42b7e9c94f1bd6dd33d3fb1d5c9e1105f014df
mapeki <mapeki268@gmail.com>
2010-12-12 17:18:36 +0000

    Some how these weren't committed before.
    lua5.1.dll is here so so tolua++.exe can run. It actually generates most of the files in this folder.
    The .tolua files are special files that the tolua++ exe uses to create the c++ code.

---
+++ Library/lua/glue/lua_meterimage.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: meter_image
+** Generated automatically by tolua++-1.0.92 on 11/28/10 09:56:41.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_meter_image_open (lua_State* tolua_S);
+
