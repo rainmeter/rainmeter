416232dff280f466221bef0340584d924fac87e9
jsmorley <jsmorley@gmail.com>
2010-12-12 15:44:14 +0000

    Update to C# InputText plugin source, to replace CS_DLL_for_C.exe with a new version from psouza4

--- revision-number.h
+++ revision-number.h
@@ -1,8 +1,8 @@
 #pragma once
-#define FILEVER        1,4,0,638
-#define PRODUCTVER     1,4,0,638
-#define STRFILEVER     "1.4.0.638"
-#define STRPRODUCTVER  "1.4.0.638"
+#define FILEVER        1,4,0,655
+#define PRODUCTVER     1,4,0,655
+#define STRFILEVER     "1.4.0.655"
+#define STRPRODUCTVER  "1.4.0.655"
 
-const int revision_number = 638;
+const int revision_number = 655;
 const bool revision_beta = true;
