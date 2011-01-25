27fce183bfd24617409706c133b8c4c316187ece
jsmorley <jsmorley@gmail.com>
2011-01-25 16:59:18 +0000

    Some fixes by psouza4 to fix position and dismiss issues with InputText Plugin Same fixes to PluginTemplate

--- revision-number.h
+++ revision-number.h
@@ -1,8 +1,8 @@
-#pragma once
-#define FILEVER        1,4,0,694
-#define PRODUCTVER     1,4,0,694
-#define STRFILEVER     "1.4.0.694"
-#define STRPRODUCTVER  "1.4.0.694"
-
-const int revision_number = 694;
-const bool revision_beta = true;
+#pragma once 
+#define FILEVER        1,4,0,709 
+#define PRODUCTVER     1,4,0,709 
+#define STRFILEVER     "1.4.0.709" 
+#define STRPRODUCTVER  "1.4.0.709" 
+ 
+const int revision_number = 709; 
+const bool revision_beta = true; 
