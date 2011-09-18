5745344c73ae6d92d7b50ec51c9462590c72dae4
Birunthan Mohanathas <birunthan@mohanathas.com>
2011-09-18 16:06:11 +0000

    Updating Version.h and Build.bat to 2.2.

--- Version.h
+++ Version.h
@@ -1,13 +1,13 @@
 #pragma once 
 #define MAKE_VER(major, minor1, minor2) major * 1000000 + minor1 * 1000 + minor2 
  
-#define FILEVER        2,1,0,757 
-#define PRODUCTVER     2,1,0,757 
-#define STRFILEVER     "2.1.0.757" 
-#define STRPRODUCTVER  "2.1.0.757" 
+#define FILEVER        2,2,0,960 
+#define PRODUCTVER     2,2,0,960 
+#define STRFILEVER     "2.2.0.960" 
+#define STRPRODUCTVER  "2.2.0.960" 
  
-#define APPVERSION L"2.1.0" 
-#define RAINMETER_VERSION MAKE_VER(2, 1, 0) 
+#define APPVERSION L"2.2.0" 
+#define RAINMETER_VERSION MAKE_VER(2, 2, 0) 
  
-const int revision_number = 757; 
+const int revision_number = 960; 
 const bool revision_beta = true; 
