678ac501ad11fb2f7ddc49e223504a8707d2072e
Birunthan Mohanathas <birunthan@mohanathas.com>
2012-09-18 22:37:35 +0300

    Added Windows 7/8 compatibility section to manifest http://msdn.microsoft.com/en-us/library/windows/desktop/hh848036.aspx

--- SkinInstaller/Application.h
+++ SkinInstaller/Application.h
@@ -23,12 +23,6 @@
 #include "zip.h"
 #include "unzip.h"
 
-#if defined _M_IX86
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#elif defined _M_X64
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#endif
-
 #define MAX_LINE_LENGTH		4096
 #define MB_ERROR			MB_OK | MB_TOPMOST | MB_ICONERROR
 
