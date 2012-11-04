642a2fae00a60e75f64594ea7d4f4afb73db22c9
jsmorley <jsmorley@gmail.com>
2012-11-04 12:02:12 -0500

    Update Version.cs

--- Version.cs
+++ Version.cs
@@ -3,9 +3,9 @@ namespace Rainmeter
     public class Version
     {
 #if X64
-        public const string Informational = "2.4.0.1499 (64-bit)";
+        public const string Informational = "2.4.0.1678 (64-bit)";
 #else
-        public const string Informational = "2.4.0.1499 (32-bit)";
+        public const string Informational = "2.4.0.1678 (32-bit)";
 #endif
     }
 }
