33b8c49d1ab8427c2258b055e6c60328ae39100e
jsmorley <jsmorley@gmail.com>
2012-11-04 12:03:24 -0500

    Update Version.h

--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 2,4,0,1499
+#define FILEVER 2,4,0,1678
 #define PRODUCTVER FILEVER
-#define STRFILEVER "2.4.0.1499"
+#define STRFILEVER "2.4.0.1678"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"2.4.0"
 #define RAINMETER_VERSION ((2 * 1000000) + (4 * 1000) + 0)
-const int revision_number = 1499;
+const int revision_number = 1678;
 const bool revision_beta = true;
