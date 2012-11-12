4986403f86ccdb96c0a8b919587bf451a82051cc
jsmorley <jsmorley@gmail.com>
2012-11-11 22:07:49 -0500

    Update Build/Build.bat

--- Build/Build.bat
+++ Build/Build.bat
@@ -8,7 +8,7 @@ set GIT=%PROGRAMFILES%\Git\bin\git.exe
 
 :: Set VERSION_REVISION to non-zero value to override
 set VERSION_MAJOR=2
-set VERSION_MINOR=4
+set VERSION_MINOR=5
 set VERSION_SUBMINOR=0
 set VERSION_REVISION=0
 set ISBETA=true
