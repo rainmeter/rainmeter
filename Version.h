ed9f27ba380119718b17ec16ee4222977601fb38
Brian Ferguson <ferguson.brian@gmail.com>
2012-11-23 12:09:56 -0700

    Finished updating to 2.5

--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 2,4,0,1678
+#define FILEVER 2,5,0,1678
 #define PRODUCTVER FILEVER
-#define STRFILEVER "2.4.0.1678"
+#define STRFILEVER "2.5.0.1678"
 #define STRPRODUCTVER STRFILEVER
-#define APPVERSION L"2.4.0"
-#define RAINMETER_VERSION ((2 * 1000000) + (4 * 1000) + 0)
+#define APPVERSION L"2.5.0"
+#define RAINMETER_VERSION ((2 * 1000000) + (5 * 1000) + 0)
 const int revision_number = 1678;
 const bool revision_beta = true;
