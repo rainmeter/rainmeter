9217cbfc35b7cfeded7176e2675a9d1fdb194dd3
jsmorley <jsmorley@gmail.com>
2013-03-25 09:44:45 -0400

    Update build to 3.0

--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 3,0,0,1842
+#define FILEVER 3,0,0,1847
 #define PRODUCTVER FILEVER
-#define STRFILEVER "3.0.0.1842"
+#define STRFILEVER "3.0.0.1847"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"3.0.0"
 #define RAINMETER_VERSION ((3 * 1000000) + (0 * 1000) + 0)
-const int revision_number = 1842;
+const int revision_number = 1847;
 const bool revision_beta = true;
