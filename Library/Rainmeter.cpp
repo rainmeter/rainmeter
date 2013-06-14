rainmeter/rainmeter

commit 04090b232a73f209a051cc9b6879fb557f0ce590

Make Rainmeter a singleton class This change is part of making the Libray project more testable. The old g_Rainmeter global pointer has been replaced with a GetRainmeter() function to guarantee that the object exists in some state.

Library/Rainmeter.cpp
@@ -45,8 +45,6 @@ enum INTERVAL
 	INTERVAL_NETSTATS = 120000
 };
 
-Rainmeter* g_Rainmeter; // The module
-
 /*
 ** Initializes Rainmeter.
 **
@@ -95,15 +93,12 @@ int RainmeterMain(LPWSTR cmdLine)
 
 	const WCHAR* iniFile = (*cmdLine && !layout) ? cmdLine : nullptr;
 
-	g_Rainmeter = new Rainmeter;
-	int ret = g_Rainmeter->Initialize(iniFile, layout);
+	auto& rainmeter = GetRainmeter();
+	int ret = rainmeter.Initialize(iniFile, layout);
 	if (ret == 0)
 	{
-		ret = g_Rainmeter->MessagePump();
+		ret = rainmeter.MessagePump();
 	}
-
-	delete g_Rainmeter;
-	g_Rainmeter = nullptr;
 
 	return ret;
 }
@@ -177,6 +172,12 @@ Rainmeter::~Rainmeter()
 	CoUninitialize();
 
 	GdiplusShutdown(m_GDIplusToken);
+}
+
+Rainmeter& Rainmeter::GetInstance()
+{
+	static Rainmeter s_Rainmeter;
+	return s_Rainmeter;
 }
 
 /*
@@ -538,7 +539,7 @@ LRESULT CALLBACK Rainmeter::MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPA
 				const WCHAR* data = (const WCHAR*)cds->lpData;
 				if (cds->dwData == 1 && (cds->cbData > 0))
 				{
-					g_Rainmeter->DelayedExecuteCommand(data);
+					GetRainmeter().DelayedExecuteCommand(data);
 				}
 			}
 		}
@@ -549,12 +550,12 @@ LRESULT CALLBACK Rainmeter::MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPA
 		{
 			MeasureNet::UpdateIFTable();
 			MeasureNet::UpdateStats();
-			g_Rainmeter->WriteStats(false);
+			GetRainmeter().WriteStats(false);
 		}
 		break;
 
 	case WM_RAINMETER_DELAYED_REFRESH_ALL:
-		g_Rainmeter->RefreshAll();
+		GetRainmeter().RefreshAll();
 		break;
 
 	case WM_RAINMETER_DELAYED_EXECUTE:
@@ -562,15 +563,15 @@ LRESULT CALLBACK Rainmeter::MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPA
 		{
 			// Execute bang
 			WCHAR* bang = (WCHAR*)lParam;
-			g_Rainmeter->ExecuteCommand(bang, nullptr);
+			GetRainmeter().ExecuteCommand(bang, nullptr);
 			free(bang);  // _wcsdup()
 		}
 		break;
 
 	case WM_RAINMETER_EXECUTE:
-		if (g_Rainmeter->HasMeterWindow((MeterWindow*)wParam))
+		if (GetRainmeter().HasMeterWindow((MeterWindow*)wParam))
 		{
-			g_Rainmeter->ExecuteCommand((const WCHAR*)lParam, (MeterWindow*)wParam);
+			GetRainmeter().ExecuteCommand((const WCHAR*)lParam, (MeterWindow*)wParam);
 		}
 		break;
 
@@ -883,7 +884,7 @@ void Rainmeter::ToggleSkin(int folderIndex, int fileIndex)
 	{
 		if (m_SkinFolders[folderIndex].active == fileIndex + 1)
 		{
-			MeterWindow* meterWindow = g_Rainmeter->GetMeterWindow(GetFolderPath(folderIndex));
+			MeterWindow* meterWindow = GetRainmeter().GetMeterWindow(GetFolderPath(folderIndex));
 			DeactivateSkin(meterWindow, folderIndex);
 		}
 		else
@@ -2035,10 +2036,10 @@ int Rainmeter::CreateAllSkinsMenuRecursive(HMENU skinMenu, int index)
 	int initialLevel = m_SkinFolders[index].level;
 	int menuIndex = 0;
 
-	const size_t max = g_Rainmeter->m_SkinFolders.size();
+	const size_t max = GetRainmeter().m_SkinFolders.size();
 	while (index < max)
 	{
-		const SkinFolder& skinFolder = g_Rainmeter->m_SkinFolders[index];
+		const SkinFolder& skinFolder = GetRainmeter().m_SkinFolders[index];
 		if (skinFolder.level != initialLevel)
 		{
 			return index - 1;

Library/Rainmeter.h
@@ -90,8 +90,7 @@ public:
 		}
 	};
 
-	Rainmeter();
-	~Rainmeter();
+	static Rainmeter& GetInstance();
 
 	int Initialize(LPCWSTR iniPath, LPCWSTR layout);
 	bool IsAlreadyRunning();
@@ -156,7 +155,7 @@ public:
 
 	HWND GetWindow() { return m_Window; }
 
-	HINSTANCE GetInstance() { return m_Instance; }
+	HINSTANCE GetModuleInstance() { return m_Instance; }
 	HINSTANCE GetResourceInstance() { return m_ResourceInstance; }
 	LCID GetResourceLCID() { return m_ResourceLCID; }
 
@@ -216,6 +215,9 @@ public:
 	friend class DialogManage;
 
 private:
+	Rainmeter();
+	~Rainmeter();
+
 	static LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 	void ActivateActiveSkins();
@@ -304,6 +306,9 @@ private:
 
 	GlobalOptions m_GlobalOptions;
 };
+
+// Convenience function.
+inline Rainmeter& GetRainmeter() { return Rainmeter::GetInstance(); }
 
 #ifdef LIBRARY_EXPORTS
 #define EXPORT_PLUGIN EXTERN_C

Library/CommandHandler.cpp
@@ -29,8 +29,6 @@
 #include "TrayWindow.h"
 #include "resource.h"
 
-extern Rainmeter* g_Rainmeter;
-
 namespace {
 
 typedef void (* BangHandlerFunc)(std::vector<std::wstring>& args, MeterWindow* skin);
@@ -160,7 +158,7 @@ void DoBang(const BangInfo& bangInfo, std::vector<std::wstring>& args, MeterWind
 				const std::wstring& folderPath = args[bangInfo.argCount];
 				if (!folderPath.empty() && (folderPath.length() != 1 || folderPath[0] != L'*'))
 				{
-					MeterWindow* skin = g_Rainmeter->GetMeterWindow(folderPath);
+					MeterWindow* skin = GetRainmeter().GetMeterWindow(folderPath);
 					if (skin)
 					{
 						skin->DoBang(bangInfo.bang, args);
@@ -174,7 +172,7 @@ void DoBang(const BangInfo& bangInfo, std::vector<std::wstring>& args, MeterWind
 			}
 
 			// No skin defined -> apply to all.
-			for (const auto& ip : g_Rainmeter->GetAllMeterWindows())
+			for (const auto& ip : GetRainmeter().GetAllMeterWindows())
 			{
 				ip.second->DoBang(bangInfo.bang, args);
 			}
@@ -208,7 +206,7 @@ void DoGroupBang(const BangInfo& bangInfo, std::vector<std::wstring>& args, Mete
 	if (args.size() > bangInfo.argCount)
 	{
 		std::multimap<int, MeterWindow*> windows;
-		g_Rainmeter->GetMeterWindowsByLoadOrder(windows, args[bangInfo.argCount]);
+		GetRainmeter().GetMeterWindowsByLoadOrder(windows, args[bangInfo.argCount]);
 
 		// Remove extra parameters (including group).
 		args.resize(bangInfo.argCount);
@@ -579,24 +577,24 @@ void CommandHandler::DoActivateSkinBang(std::vector<std::wstring>& args, MeterWi
 {
 	if (args.size() == 1)
 	{
-		int index = g_Rainmeter->FindSkinFolderIndex(args[0]);
+		int index = GetRainmeter().FindSkinFolderIndex(args[0]);
 		if (index != -1)
 		{
-			const Rainmeter::SkinFolder& skinFolder = g_Rainmeter->m_SkinFolders[index];
+			const Rainmeter::SkinFolder& skinFolder = GetRainmeter().m_SkinFolders[index];
 			if (!(skinFolder.active == 1 && skinFolder.files.size() == 1))
 			{
 				// Activate the next index.
-				g_Rainmeter->ActivateSkin(index, (skinFolder.active < skinFolder.files.size()) ? skinFolder.active : 0);
+				GetRainmeter().ActivateSkin(index, (skinFolder.active < skinFolder.files.size()) ? skinFolder.active : 0);
 			}
 			return;
 		}
 	}
 	else if (args.size() > 1)
 	{
-		std::pair<int, int> indexes = g_Rainmeter->GetMeterWindowIndex(args[0], args[1]);
+		std::pair<int, int> indexes = GetRainmeter().GetMeterWindowIndex(args[0], args[1]);
 		if (indexes.first != -1 && indexes.second != -1)
 		{
-			g_Rainmeter->ActivateSkin(indexes.first, indexes.second);
+			GetRainmeter().ActivateSkin(indexes.first, indexes.second);
 			return;
 		}
 	}
@@ -608,7 +606,7 @@ void CommandHandler::DoDeactivateSkinBang(std::vector<std::wstring>& args, Meter
 {
 	if (!args.empty())
 	{
-		skin = g_Rainmeter->GetMeterWindow(args[0]);
+		skin = GetRainmeter().GetMeterWindow(args[0]);
 		if (!skin)
 		{
 			LogWarningF(L"!DeactivateConfig: \"%s\" not active", args[0].c_str());
@@ -618,7 +616,7 @@ void CommandHandler::DoDeactivateSkinBang(std::vector<std::wstring>& args, Meter
 
 	if (skin)
 	{
-		g_Rainmeter->DeactivateSkin(skin, -1);
+		GetRainmeter().DeactivateSkin(skin, -1);
 	}
 	else
 	{
@@ -630,10 +628,10 @@ void CommandHandler::DoToggleSkinBang(std::vector<std::wstring>& args, MeterWind
 {
 	if (args.size() >= 2)
 	{
-		MeterWindow* skin = g_Rainmeter->GetMeterWindow(args[0]);
+		MeterWindow* skin = GetRainmeter().GetMeterWindow(args[0]);
 		if (skin)
 		{
-			g_Rainmeter->DeactivateSkin(skin, -1);
+			GetRainmeter().DeactivateSkin(skin, -1);
 			return;
 		}
 
@@ -651,10 +649,10 @@ void CommandHandler::DoDeactivateSkinGroupBang(std::vector<std::wstring>& args, 
 	if (!args.empty())
 	{
 		std::multimap<int, MeterWindow*> windows;
-		g_Rainmeter->GetMeterWindowsByLoadOrder(windows, args[0]);
+		GetRainmeter().GetMeterWindowsByLoadOrder(windows, args[0]);
 		for (const auto& ip : windows)
 		{
-			g_Rainmeter->DeactivateSkin(ip.second, -1);
+			GetRainmeter().DeactivateSkin(ip.second, -1);
 		}
 	}
 	else
@@ -673,12 +671,12 @@ void CommandHandler::DoLoadLayoutBang(std::vector<std::wstring>& args, MeterWind
 			std::wstring command = L"!LoadLayout \"";
 			command += args[0];
 			command += L'"';
-			g_Rainmeter->DelayedExecuteCommand(command.c_str());
+			GetRainmeter().DelayedExecuteCommand(command.c_str());
 		}
 		else
 		{
 			// Not called from a skin (or called with delay).
-			g_Rainmeter->LoadLayout(args[0]);
+			GetRainmeter().LoadLayout(args[0]);
 		}
 	}
 }
@@ -730,7 +728,7 @@ void CommandHandler::DoSkinMenuBang(std::vector<std::wstring>& args, MeterWindow
 {
 	if (!args.empty())
 	{
-		skin = g_Rainmeter->GetMeterWindow(args[0]);
+		skin = GetRainmeter().GetMeterWindow(args[0]);
 		if (!skin)
 		{
 			LogWarningF(L"!SkinMenu: \"%s\" not active", args[0].c_str());
@@ -741,7 +739,7 @@ void CommandHandler::DoSkinMenuBang(std::vector<std::wstring>& args, MeterWindow
 	if (skin)
 	{
 		POINT pos = System::GetCursorPosition();
-		g_Rainmeter->ShowContextMenu(pos, skin);
+		GetRainmeter().ShowContextMenu(pos, skin);
 	}
 	else
 	{
@@ -752,12 +750,12 @@ void CommandHandler::DoSkinMenuBang(std::vector<std::wstring>& args, MeterWindow
 void CommandHandler::DoTrayMenuBang(std::vector<std::wstring>& args, MeterWindow* skin)
 {
 	POINT pos = System::GetCursorPosition();
-	g_Rainmeter->ShowContextMenu(pos, nullptr);
+	GetRainmeter().ShowContextMenu(pos, nullptr);
 }
 
 void CommandHandler::DoResetStatsBang(std::vector<std::wstring>& args, MeterWindow* meterWindow)
 {
-	g_Rainmeter->ResetStats();
+	GetRainmeter().ResetStats();
 }
 
 void CommandHandler::DoWriteKeyValueBang(std::vector<std::wstring>& args, MeterWindow* skin)
@@ -787,8 +785,8 @@ void CommandHandler::DoWriteKeyValueBang(std::vector<std::wstring>& args, MeterW
 		return;
 	}
 
-	if (_wcsnicmp(iniFile, g_Rainmeter->m_SkinPath.c_str(), g_Rainmeter->m_SkinPath.size()) != 0 &&
-		_wcsnicmp(iniFile, g_Rainmeter->m_SettingsPath.c_str(), g_Rainmeter->m_SettingsPath.size()) != 0)
+	if (_wcsnicmp(iniFile, GetRainmeter().m_SkinPath.c_str(), GetRainmeter().m_SkinPath.size()) != 0 &&
+		_wcsnicmp(iniFile, GetRainmeter().m_SettingsPath.c_str(), GetRainmeter().m_SettingsPath.size()) != 0)
 	{
 		LogErrorF(L"!WriteKeyValue: Illegal path: %s", iniFile);
 		return;
@@ -821,14 +819,14 @@ void CommandHandler::DoWriteKeyValueBang(std::vector<std::wstring>& args, MeterW
 
 	if (temporary)
 	{
-		if (g_Rainmeter->GetDebug())
+		if (GetRainmeter().GetDebug())
 		{
 			LogDebugF(L"!WriteKeyValue: Writing to: %s (Temp: %s)", iniFile, strIniWrite.c_str());
 		}
 	}
 	else
 	{
-		if (g_Rainmeter->GetDebug())
+		if (GetRainmeter().GetDebug())
 		{
 			LogDebugF(L"!WriteKeyValue: Writing to: %s", iniFile);
 		}
@@ -925,13 +923,13 @@ void CommandHandler::DoLogBang(std::vector<std::wstring>& args, MeterWindow* ski
 void CommandHandler::DoRefreshApp(std::vector<std::wstring>& args, MeterWindow* meterWindow)
 {
 	// Refresh needs to be delayed since it crashes if done during Update().
-	PostMessage(g_Rainmeter->m_Window, WM_RAINMETER_DELAYED_REFRESH_ALL, 0, 0);
+	PostMessage(GetRainmeter().m_Window, WM_RAINMETER_DELAYED_REFRESH_ALL, 0, 0);
 }
 
 void CommandHandler::DoQuitBang(std::vector<std::wstring>& args, MeterWindow* meterWindow)
 {
 	// Quit needs to be delayed since it crashes if done during Update().
-	PostMessage(g_Rainmeter->GetTrayWindow()->GetWindow(), WM_COMMAND, MAKEWPARAM(IDM_QUIT, 0), 0);
+	PostMessage(GetRainmeter().GetTrayWindow()->GetWindow(), WM_COMMAND, MAKEWPARAM(IDM_QUIT, 0), 0);
 }
 
 void CommandHandler::DoLsBoxHookBang(std::vector<std::wstring>& args, MeterWindow* meterWindow)

Library/ConfigParser.cpp
@@ -27,8 +27,6 @@
 #include "Meter.h"
 #include "resource.h"
 
-extern Rainmeter* g_Rainmeter;
-
 using namespace Gdiplus;
 
 std::unordered_map<std::wstring, std::wstring> ConfigParser::c_MonitorVariables;
@@ -94,13 +92,13 @@ void ConfigParser::SetBuiltInVariables(const std::wstring& filename, const std::
 		return m_BuiltInVariables.insert(std::make_pair(name, value));
 	};
 
-	insertVariable(L"PROGRAMPATH", g_Rainmeter->GetPath());
-	insertVariable(L"PROGRAMDRIVE", g_Rainmeter->GetDrive());
-	insertVariable(L"SETTINGSPATH", g_Rainmeter->GetSettingsPath());
-	insertVariable(L"SKINSPATH", g_Rainmeter->GetSkinPath());
-	insertVariable(L"PLUGINSPATH", g_Rainmeter->GetPluginPath());
+	insertVariable(L"PROGRAMPATH", GetRainmeter().GetPath());
+	insertVariable(L"PROGRAMDRIVE", GetRainmeter().GetDrive());
+	insertVariable(L"SETTINGSPATH", GetRainmeter().GetSettingsPath());
+	insertVariable(L"SKINSPATH", GetRainmeter().GetSkinPath());
+	insertVariable(L"PLUGINSPATH", GetRainmeter().GetPluginPath());
 	insertVariable(L"CURRENTPATH", PathUtil::GetFolderFromFilePath(filename));
-	insertVariable(L"ADDONSPATH", g_Rainmeter->GetAddonPath());
+	insertVariable(L"ADDONSPATH", GetRainmeter().GetAddonPath());
 
 	if (meterWindow)
 	{
@@ -1289,7 +1287,7 @@ void ConfigParser::ReadIniFile(const std::wstring& iniFile, LPCTSTR skinSection,
 {
 	if (depth > 100)	// Is 100 enough to assume the include loop never ends?
 	{
-		g_Rainmeter->ShowMessage(nullptr, GetString(ID_STR_INCLUDEINFINITELOOP), MB_OK | MB_ICONERROR);
+		GetRainmeter().ShowMessage(nullptr, GetString(ID_STR_INCLUDEINFINITELOOP), MB_OK | MB_ICONERROR);
 		return;
 	}
 
@@ -1306,11 +1304,11 @@ void ConfigParser::ReadIniFile(const std::wstring& iniFile, LPCTSTR skinSection,
 
 	if (temporary)
 	{
-		if (g_Rainmeter->GetDebug()) LogDebugF(L"Reading file: %s (Temp: %s)", iniFile.c_str(), iniRead.c_str());
+		if (GetRainmeter().GetDebug()) LogDebugF(L"Reading file: %s (Temp: %s)", iniFile.c_str(), iniRead.c_str());
 	}
 	else
 	{
-		if (g_Rainmeter->GetDebug()) LogDebugF(L"Reading file: %s", iniFile.c_str());
+		if (GetRainmeter().GetDebug()) LogDebugF(L"Reading file: %s", iniFile.c_str());
 		iniRead = iniFile;
 	}
 

Library/DialogAbout.cpp
@@ -26,8 +26,6 @@
 #include "DialogAbout.h"
 #include "../Version.h"
 
-extern Rainmeter* g_Rainmeter;
-
 WINDOWPLACEMENT DialogAbout::c_WindowPlacement = {0};
 DialogAbout* DialogAbout::c_Dialog = nullptr;
 
@@ -63,7 +61,7 @@ void DialogAbout::Open(int tab)
 		0, 0, 400, 210,
 		DS_CENTER | WS_POPUP | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME,
 		WS_EX_APPWINDOW | WS_EX_CONTROLPARENT | ((*GetString(ID_STR_ISRTL) == L'1') ? WS_EX_LAYOUTRTL : 0),
-		g_Rainmeter->GetWindow());
+		GetRainmeter().GetWindow());
 
 	// Fake WM_NOTIFY to change tab
 	NMHDR nm;
@@ -702,7 +700,7 @@ void DialogAbout::TabSkins::UpdateSkinList()
 
 	// Add entries for each skin
 	std::wstring::size_type maxLength = 0;
-	const std::map<std::wstring, MeterWindow*>& windows = g_Rainmeter->GetAllMeterWindows();
+	const std::map<std::wstring, MeterWindow*>& windows = GetRainmeter().GetAllMeterWindows();
 	std::map<std::wstring, MeterWindow*>::const_iterator iter = windows.begin();
 	bool found = false;
 	for ( ; iter != windows.end(); ++iter)
@@ -756,7 +754,7 @@ void DialogAbout::TabSkins::UpdateMeasureList(MeterWindow* meterWindow)
 		HWND item = GetControl(Id_SkinsListBox);
 		int selected = (int)SendMessage(item, LB_GETCURSEL, 0, 0);
 
-		const std::map<std::wstring, MeterWindow*>& windows = g_Rainmeter->GetAllMeterWindows();
+		const std::map<std::wstring, MeterWindow*>& windows = GetRainmeter().GetAllMeterWindows();
 		std::map<std::wstring, MeterWindow*>::const_iterator iter = windows.begin();
 		while (selected && iter != windows.end())
 		{
@@ -1090,10 +1088,10 @@ void DialogAbout::TabPlugins::Initialize()
 		FindClose(hSearch);
 	};
 
-	findPlugins(g_Rainmeter->GetPluginPath());
-	if (g_Rainmeter->HasUserPluginPath())
+	findPlugins(GetRainmeter().GetPluginPath());
+	if (GetRainmeter().HasUserPluginPath())
 	{
-		findPlugins(g_Rainmeter->GetUserPluginPath());
+		findPlugins(GetRainmeter().GetUserPluginPath());
 	}
 
 	m_Initialized = true;
@@ -1181,15 +1179,15 @@ void DialogAbout::TabVersion::Initialize()
 	SetWindowText(item, tmpSz);
 
 	item = GetControl(Id_PathLabel);
-	std::wstring text = L"Path: " + g_Rainmeter->GetPath();
+	std::wstring text = L"Path: " + GetRainmeter().GetPath();
 	SetWindowText(item, text.c_str());
 
 	item = GetControl(Id_IniFileLabel);
-	text = L"IniFile: " + g_Rainmeter->GetIniFile();
+	text = L"IniFile: " + GetRainmeter().GetIniFile();
 	SetWindowText(item, text.c_str());
 
 	item = GetControl(Id_SkinPathLabel);
-	text = L"SkinPath: " + g_Rainmeter->GetSkinPath();
+	text = L"SkinPath: " + GetRainmeter().GetSkinPath();
 	SetWindowText(item, text.c_str());
 
 	m_Initialized = true;
@@ -1228,11 +1226,11 @@ INT_PTR DialogAbout::TabVersion::OnCommand(WPARAM wParam, LPARAM lParam)
 			int len = _snwprintf_s(tmpSz, _TRUNCATE, L"%s%s r%i %s (%s)", APPVERSION, revision_beta ? L" beta" : L"", revision_number, APPBITS, APPDATE);
 			std::wstring text(tmpSz, len);
 			text += L"\nPath: ";
-			text += g_Rainmeter->GetPath();
+			text += GetRainmeter().GetPath();
 			text += L"\nIniFile: ";
-			text += g_Rainmeter->GetIniFile();
+			text += GetRainmeter().GetIniFile();
 			text += L"\nSkinPath: ";
-			text += g_Rainmeter->GetSkinPath();
+			text += GetRainmeter().GetSkinPath();
 			System::SetClipboardText(text);
 		}
 		break;

commit 9a62ebef9595475748b1d67f94081e045dbe96d1

Fix crash caused by 04090b2

Library/Rainmeter.cpp
@@ -99,6 +99,7 @@ int RainmeterMain(LPWSTR cmdLine)
 	{
 		ret = rainmeter.MessagePump();
 	}
+	rainmeter.Finalize();
 
 	return ret;
 }
@@ -143,32 +144,6 @@ Rainmeter::Rainmeter() :
 */
 Rainmeter::~Rainmeter()
 {
-	KillTimer(m_Window, TIMER_NETSTATS);
-
-	DeleteAllUnmanagedMeterWindows();
-	DeleteAllMeterWindows();
-
-	delete m_TrayWindow;
-
-	System::Finalize();
-
-	MeasureNet::UpdateIFTable();
-	MeasureNet::UpdateStats();
-	WriteStats(true);
-
-	MeasureNet::FinalizeStatic();
-	MeasureCPU::FinalizeStatic();
-	MeterString::FinalizeStatic();
-
-	// Change the work area back
-	if (m_DesktopWorkAreaChanged)
-	{
-		UpdateDesktopWorkArea(true);
-	}
-
-	if (m_ResourceInstance) FreeLibrary(m_ResourceInstance);
-	if (m_Mutex) ReleaseMutex(m_Mutex);
-
 	CoUninitialize();
 
 	GdiplusShutdown(m_GDIplusToken);
@@ -439,6 +414,35 @@ int Rainmeter::Initialize(LPCWSTR iniPath, LPCWSTR layout)
 	}
 
 	return 0;	// All is OK
+}
+
+void Rainmeter::Finalize()
+{
+	KillTimer(m_Window, TIMER_NETSTATS);
+
+	DeleteAllUnmanagedMeterWindows();
+	DeleteAllMeterWindows();
+
+	delete m_TrayWindow;
+
+	System::Finalize();
+
+	MeasureNet::UpdateIFTable();
+	MeasureNet::UpdateStats();
+	WriteStats(true);
+
+	MeasureNet::FinalizeStatic();
+	MeasureCPU::FinalizeStatic();
+	MeterString::FinalizeStatic();
+
+	// Change the work area back
+	if (m_DesktopWorkAreaChanged)
+	{
+		UpdateDesktopWorkArea(true);
+	}
+
+	if (m_ResourceInstance) FreeLibrary(m_ResourceInstance);
+	if (m_Mutex) ReleaseMutex(m_Mutex);
 }
 
 bool Rainmeter::IsAlreadyRunning()

Library/Rainmeter.h
@@ -93,6 +93,8 @@ public:
 	static Rainmeter& GetInstance();
 
 	int Initialize(LPCWSTR iniPath, LPCWSTR layout);
+	void Finalize();
+
 	bool IsAlreadyRunning();
 	int MessagePump();
 