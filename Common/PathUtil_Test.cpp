096023ba6e59d99b873fcd5986baecc287585bd5
Birunthan Mohanathas <birunthan@mohanathas.com>
2013-06-14 16:58:53 +0300

    Cosmetics

--- Common/PathUtil_Test.cpp
+++ Common/PathUtil_Test.cpp
@@ -16,8 +16,8 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include "CppUnitTest.h"
 #include "PathUtil.h"
+#include <CppUnitTest.h>
 #include <Shlobj.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
--- Library/ConfigParser_Test.cpp
+++ Library/ConfigParser_Test.cpp
@@ -16,8 +16,8 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include "CppUnitTest.h"
 #include "ConfigParser.h"
+#include <CppUnitTest.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
