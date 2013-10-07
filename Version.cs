38bc067317798da8c413406445642805f3f7a8e1
Aaron <e0x539@gmail.com>
2013-10-06 22:49:07 -0400

    Upstream Master

--- Version.cs
+++ Version.cs
@@ -3,9 +3,9 @@ namespace Rainmeter
     public class Version
     {
 #if X64
-        public const string Informational = "3.0.0.1847 (64-bit)";
+        public const string Informational = "3.0.0.2107 (64-bit)";
 #else
-        public const string Informational = "3.0.0.1847 (32-bit)";
+        public const string Informational = "3.0.0.2107 (32-bit)";
 #endif
     }
 }
