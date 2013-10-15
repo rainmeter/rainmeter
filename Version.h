d6e26401cabe53ca259e78a5d944d1bcfe3b9ae0
Birunthan Mohanathas <birunthan@mohanathas.com>
2013-10-15 17:16:43 +0300

    Update Version.{h,cs} files for 3.0.1

--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 3,0,0,1847
+#define FILEVER 3,0,1,2121
 #define PRODUCTVER FILEVER
-#define STRFILEVER "3.0.0.1847"
+#define STRFILEVER "3.0.1.2121"
 #define STRPRODUCTVER STRFILEVER
-#define APPVERSION L"3.0.0"
-#define RAINMETER_VERSION ((3 * 1000000) + (0 * 1000) + 0)
-const int revision_number = 1847;
+#define APPVERSION L"3.0.1"
+#define RAINMETER_VERSION ((3 * 1000000) + (0 * 1000) + 1)
+const int revision_number = 2121;
 const bool revision_beta = true;
