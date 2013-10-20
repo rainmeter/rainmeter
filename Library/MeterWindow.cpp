rainmeter/rainmeter

commit ef1cf223a720166cf27fa93d940b0287caaa6451

Change to use Gfx rendering

Library/MeterWindow.cpp
@@ -36,6 +36,8 @@
 #include "TintedImage.h"
 #include "MeasureScript.h"
 #include "../Version.h"
+#include "../Common/Gfx/CanvasGDIP.h"
+#include "../Common/Gfx/CanvasD2D.h"
 
 using namespace Gdiplus;
 
@@ -74,11 +76,7 @@ extern CRainmeter* Rainmeter;
 **
 */
 CMeterWindow::CMeterWindow(const std::wstring& folderPath, const std::wstring& file) : m_FolderPath(folderPath), m_FileName(file),
-	m_DoubleBuffer(),
-	m_DIBSectionBuffer(),
-	m_DIBSectionBufferPixels(),
-	m_DIBSectionBufferW(),
-	m_DIBSectionBufferH(),
+	m_Canvas(),
 	m_Background(),
 	m_BackgroundSize(),
 	m_Window(),
@@ -145,6 +143,9 @@ CMeterWindow::CMeterWindow(const std::wstring& folderPath, const std::wstring& f
 	m_FontCollection(),
 	m_ToolTipHidden(false)
 {
+	m_Canvas = Platform::IsAtLeastWinVista() ?
+		(Gfx::Canvas*)new Gfx::CanvasD2D() : (Gfx::Canvas*)new Gfx::CanvasGDIP();
+
 	if (!c_DwmInstance && Platform::IsAtLeastWinVista())
 	{
 		c_DwmInstance = CSystem::RmLoadLibrary(L"dwmapi.dll");
@@ -201,6 +202,9 @@ CMeterWindow::~CMeterWindow()
 			c_DwmIsCompositionEnabled = NULL;
 		}
 	}
+
+	delete m_Canvas;
+	m_Canvas = NULL;
 }
 
 /*
@@ -260,16 +264,6 @@ void CMeterWindow::Dispose(bool refresh)
 
 	if (!refresh)
 	{
-		// Destroy double buffers and window
-		delete m_DoubleBuffer;
-		m_DoubleBuffer = NULL;
-
-		if (m_DIBSectionBuffer)
-		{
-			DeleteObject(m_DIBSectionBuffer);
-			m_DIBSectionBuffer = NULL;
-		}
-
 		if (m_Window)
 		{
 			DestroyWindow(m_Window);
@@ -2439,25 +2433,7 @@ bool CMeterWindow::ResizeWindow(bool reset)
 */
 void CMeterWindow::CreateDoubleBuffer(int cx, int cy)
 {
-	// Create DIBSection bitmap
-	BITMAPV4HEADER bmiHeader = {sizeof(BITMAPV4HEADER)};
-	bmiHeader.bV4Width = cx;
-	bmiHeader.bV4Height = -cy;  // top-down DIB
-	bmiHeader.bV4Planes = 1;
-	bmiHeader.bV4BitCount = 32;
-	bmiHeader.bV4V4Compression = BI_BITFIELDS;
-	bmiHeader.bV4RedMask = 0x00FF0000;
-	bmiHeader.bV4GreenMask = 0x0000FF00;
-	bmiHeader.bV4BlueMask = 0x000000FF;
-	bmiHeader.bV4AlphaMask = 0xFF000000;
-
-	m_DIBSectionBuffer = CreateDIBSection(NULL, (BITMAPINFO*)&bmiHeader, DIB_RGB_COLORS, (void**)&m_DIBSectionBufferPixels, NULL, 0);
-
-	// Create GDI+ bitmap from DIBSection's pixels
-	m_DoubleBuffer = new Bitmap(cx, cy, cx * 4, PixelFormat32bppPARGB, (BYTE*)m_DIBSectionBufferPixels);
-
-	m_DIBSectionBufferW = cx;
-	m_DIBSectionBufferH = cy;
+	m_Canvas->Resize(cx, cy);
 }
 
 /*
@@ -2484,29 +2460,26 @@ void CMeterWindow::Redraw()
 			cy = 1;
 		}
 
-		if (cx != m_DIBSectionBufferW || cy != m_DIBSectionBufferH || m_DIBSectionBufferPixels == NULL)
+		if (cx != m_Canvas->GetW() || cy != m_Canvas->GetH())
 		{
-			delete m_DoubleBuffer;
-			if (m_DIBSectionBuffer) DeleteObject(m_DIBSectionBuffer);
-			m_DIBSectionBufferPixels = NULL;
-
 			CreateDoubleBuffer(cx, cy);
-		}
-		else
-		{
-			memset(m_DIBSectionBufferPixels, 0, cx * cy * 4);
 		}
 	}
 
+	if (!m_Canvas->BeginDraw())
+	{
+		return;
+	}
+
+	m_Canvas->Clear();
+
 	if (m_WindowW != 0 && m_WindowH != 0)
 	{
-		Graphics graphics(m_DoubleBuffer);
-
 		if (m_Background)
 		{
-			// Copy the background over the doublebuffer
-			Rect r(0, 0, m_WindowW, m_WindowH);
-			graphics.DrawImage(m_Background, r, 0, 0, m_Background->GetWidth(), m_Background->GetHeight(), UnitPixel);
+			const Rect dst(0, 0, m_WindowW, m_WindowH);
+			const Rect src(0, 0, m_Background->GetWidth(), m_Background->GetHeight());
+			m_Canvas->DrawBitmap(m_Background, dst, src);
 		}
 		else if (m_BackgroundMode == BGMODE_SOLID)
 		{
@@ -2517,12 +2490,14 @@ void CMeterWindow::Redraw()
 			{
 				if (m_SolidColor.GetValue() == m_SolidColor2.GetValue())
 				{
-					graphics.Clear(m_SolidColor);
+					m_Canvas->Clear(m_SolidColor);
 				}
 				else
 				{
+					Gdiplus::Graphics& graphics = m_Canvas->BeginGdiplusContext();
 					LinearGradientBrush gradient(r, m_SolidColor, m_SolidColor2, m_SolidAngle, TRUE);
 					graphics.FillRectangle(&gradient, r);
+					m_Canvas->EndGdiplusContext();
 				}
 			}
 
@@ -2540,7 +2515,9 @@ void CMeterWindow::Redraw()
 				Pen light(lightColor);
 				Pen dark(darkColor);
 
+				Gdiplus::Graphics& graphics = m_Canvas->BeginGdiplusContext();
 				CMeter::DrawBevel(graphics, r, light, dark);
+				m_Canvas->EndGdiplusContext();
 			}
 		}
 
@@ -2551,22 +2528,24 @@ void CMeterWindow::Redraw()
 			const Matrix* matrix = (*j)->GetTransformationMatrix();
 			if (matrix && !matrix->IsIdentity())
 			{
-				// Change the world matrix
-				graphics.SetTransform(matrix);
+				// TODO FIXME: Change the world matrix
+				//m_Canvas->GetGraphics().SetTransform(matrix);
 
-				(*j)->Draw(graphics);
+				(*j)->Draw(*m_Canvas);
 
-				// Set back to identity matrix
-				graphics.ResetTransform();
+				// TODO FIXME: Set back to identity matrix
+				//m_Canvas->GetGraphics().ResetTransform();
 			}
 			else
 			{
-				(*j)->Draw(graphics);
+				(*j)->Draw(*m_Canvas);
 			}
 		}
 	}
 
-	UpdateWindow(m_TransparencyValue, false);
+	UpdateWindow(m_TransparencyValue, false, true);
+
+	m_Canvas->EndDraw();
 }
 
 /*
@@ -2738,7 +2717,7 @@ void CMeterWindow::Update(bool refresh)
 ** Updates the window contents
 **
 */
-void CMeterWindow::UpdateWindow(int alpha, bool reset)
+void CMeterWindow::UpdateWindow(int alpha, bool reset, bool canvasBeginDrawCalled)
 {
 	if (reset)
 	{
@@ -2746,25 +2725,25 @@ void CMeterWindow::UpdateWindow(int alpha, bool reset)
 	}
 
 	BLENDFUNCTION blendPixelFunction = {AC_SRC_OVER, 0, alpha, AC_SRC_ALPHA};
-	POINT ptWindowScreenPosition = {m_ScreenX, m_ScreenY};
 	POINT ptSrc = {0, 0};
-	SIZE szWindow = {m_DIBSectionBufferW, m_DIBSectionBufferH};
+	SIZE szWindow = {m_WindowW, m_WindowH};
 
-	HDC dcScreen = GetDC(0);
-	HDC dcMemory = CreateCompatibleDC(dcScreen);
-	SelectObject(dcMemory, m_DIBSectionBuffer);
+	if (!canvasBeginDrawCalled) m_Canvas->BeginDraw();
 
-	BOOL ret = UpdateLayeredWindow(m_Window, dcScreen, &ptWindowScreenPosition, &szWindow, dcMemory, &ptSrc, 0, &blendPixelFunction, ULW_ALPHA);
-	if (!ret)
+	HDC dcScreen = GetDC(NULL);
+	HDC dcMemory = m_Canvas->GetDC();
+	if (!UpdateLayeredWindow(m_Window, dcScreen, NULL, &szWindow, dcMemory, &ptSrc, 0, &blendPixelFunction, ULW_ALPHA))
 	{
-		// Retry after resetting WS_EX_LAYERED flag
+		// Retry after resetting WS_EX_LAYERED flag.
 		RemoveWindowExStyle(WS_EX_LAYERED);
 		AddWindowExStyle(WS_EX_LAYERED);
-		UpdateLayeredWindow(m_Window, dcScreen, &ptWindowScreenPosition, &szWindow, dcMemory, &ptSrc, 0, &blendPixelFunction, ULW_ALPHA);
+		UpdateLayeredWindow(m_Window, dcScreen, NULL, &szWindow, dcMemory, &ptSrc, 0, &blendPixelFunction, ULW_ALPHA);
 	}
 
-	ReleaseDC(0, dcScreen);
-	DeleteDC(dcMemory);
+	ReleaseDC(NULL, dcScreen);
+	m_Canvas->ReleaseDC(dcMemory);
+
+	if (!canvasBeginDrawCalled) m_Canvas->EndDraw();
 
 	m_TransparencyValue = alpha;
 }
@@ -3066,21 +3045,7 @@ HWND CMeterWindow::GetWindowFromPoint(POINT pos)
 */
 bool CMeterWindow::HitTest(int x, int y)
 {
-	if (x >= 0 && y >= 0 && x < m_WindowW && y < m_WindowH)
-	{
-		// Check transparent pixels
-		if (m_DIBSectionBufferPixels)
-		{
-			DWORD pixel = m_DIBSectionBufferPixels[y * m_WindowW + x];  // top-down DIB
-			return ((pixel & 0xFF000000) != 0);
-		}
-		else
-		{
-			return true;
-		}
-	}
-
-	return false;
+	return m_Canvas->IsTransparentPixel(x, y);
 }
 
 /*

Library/Meter.cpp
@@ -30,6 +30,7 @@
 #include "MeterButton.h"
 #include "Measure.h"
 #include "Rainmeter.h"
+#include "../Common/Gfx/Canvas.h"
 
 using namespace Gdiplus;
 
@@ -638,23 +639,11 @@ void CMeter::UpdateToolTip()
 /*
 ** Draws the solid background & bevel if such are defined
 */
-bool CMeter::Draw(Graphics& graphics)
+bool CMeter::Draw(Gfx::Canvas& canvas)
 {
 	if (IsHidden()) return false;
 
-	graphics.SetInterpolationMode(InterpolationModeDefault);
-	graphics.SetCompositingQuality(CompositingQualityDefault);
-
-	if (m_AntiAlias)
-	{
-		graphics.SetSmoothingMode(SmoothingModeHighQuality);
-		graphics.SetPixelOffsetMode(PixelOffsetModeHighQuality);
-	}
-	else
-	{
-		graphics.SetSmoothingMode(SmoothingModeNone);
-		graphics.SetPixelOffsetMode(PixelOffsetModeDefault);
-	}
+	canvas.SetAntiAliasing(m_AntiAlias);
 
 	if (m_SolidColor.GetA() != 0 || m_SolidColor2.GetA() != 0)
 	{
@@ -666,10 +655,12 @@ bool CMeter::Draw(Graphics& graphics)
 		if (m_SolidColor.GetValue() == m_SolidColor2.GetValue())
 		{
 			SolidBrush solid(m_SolidColor);
-			graphics.FillRectangle(&solid, r);
+			canvas.FillRectangle(r, solid);
 		}
 		else
 		{
+			Gdiplus::Graphics& graphics = canvas.BeginGdiplusContext();
+
 			if (!m_AntiAlias)
 			{
 				// Fix the tiling issue in some GradientAngle values
@@ -683,11 +674,15 @@ bool CMeter::Draw(Graphics& graphics)
 			{
 				graphics.SetPixelOffsetMode(PixelOffsetModeDefault);
 			}
+
+			canvas.EndGdiplusContext();
 		}
 	}
 
 	if (m_SolidBevel != BEVELTYPE_NONE)
 	{
+		Gdiplus::Graphics& graphics = canvas.BeginGdiplusContext();
+
 		int x = GetX();
 		int y = GetY();
 
@@ -706,6 +701,8 @@ bool CMeter::Draw(Graphics& graphics)
 		// The bevel is drawn outside the meter
 		Rect rect(x - 2, y - 2, m_W + 4, m_H + 4);
 		DrawBevel(graphics, rect, light, dark);
+
+		canvas.EndGdiplusContext();
 	}
 
 	return true;

Library/Meter.h
@@ -40,7 +40,7 @@ public:
 
 	virtual void Initialize();
 	virtual bool Update();
-	virtual bool Draw(Gdiplus::Graphics& graphics);
+	virtual bool Draw(Gfx::Canvas& canvas);
 	virtual bool HasActiveTransition() { return false; }
 
 	virtual int GetW() { return m_Hidden ? 0 : m_W; }

Library/MeterBar.cpp
@@ -22,6 +22,7 @@
 #include "Error.h"
 #include "Litestep.h"
 #include "Rainmeter.h"
+#include "../Common/Gfx/Canvas.h"
 
 using namespace Gdiplus;
 
@@ -160,9 +161,9 @@ bool CMeterBar::Update()
 ** Draws the meter on the double buffer
 **
 */
-bool CMeterBar::Draw(Graphics& graphics)
+bool CMeterBar::Draw(Gfx::Canvas& canvas)
 {
-	if (!CMeter::Draw(graphics)) return false;
+	if (!CMeter::Draw(canvas)) return false;
 
 	int x = GetX();
 	int y = GetY();
@@ -183,28 +184,26 @@ bool CMeterBar::Draw(Graphics& graphics)
 				if (m_Border > 0)
 				{
 					Rect r2(x, y, m_W, m_Border);
-					graphics.DrawImage(drawBitmap, r2, 0, 0, m_W, m_Border, UnitPixel);
+					canvas.DrawBitmap(drawBitmap, r2, Rect(0, 0, m_W, m_Border));
 					r2.Y = y + size + m_Border;
-					graphics.DrawImage(drawBitmap, r2, 0, m_H - m_Border, m_W, m_Border, UnitPixel);
+					canvas.DrawBitmap(drawBitmap, r2, Rect(0, m_H - m_Border, m_W, m_Border));
 				}
 
-				// Blit the image
 				Rect r(x, y + m_Border, m_W, size);
-				graphics.DrawImage(drawBitmap, r, 0, m_Border, m_W, size, UnitPixel);
+				canvas.DrawBitmap(drawBitmap, r, Rect(0, m_Border, m_W, size));
 			}
 			else
 			{
 				if (m_Border > 0)
 				{
 					Rect r2(x, y + m_H - size - 2 * m_Border, m_W, m_Border);
-					graphics.DrawImage(drawBitmap, r2, 0, 0, m_W, m_Border, UnitPixel);
+					canvas.DrawBitmap(drawBitmap, r2, Rect(0, 0, m_W, m_Border));
 					r2.Y = y + m_H - m_Border;
-					graphics.DrawImage(drawBitmap, r2, 0, m_H - m_Border, m_W, m_Border, UnitPixel);
+					canvas.DrawBitmap(drawBitmap, r2, Rect(0, m_H - m_Border, m_W, m_Border));
 				}
 
-				// Blit the image
 				Rect r(x, y + m_H - size - m_Border, m_W, size);
-				graphics.DrawImage(drawBitmap, r, 0, m_H - size - m_Border, m_W, size, UnitPixel);
+				canvas.DrawBitmap(drawBitmap, r, Rect(0, m_H - size - m_Border, m_W, size));
 			}
 		}
 		else
@@ -213,12 +212,12 @@ bool CMeterBar::Draw(Graphics& graphics)
 			if (m_Flip)
 			{
 				Rect r(x, y, m_W, size);
-				graphics.FillRectangle(&brush, r);
+				canvas.FillRectangle(r, brush);
 			}
 			else
 			{
 				Rect r(x, y + m_H - size, m_W, size);
-				graphics.FillRectangle(&brush, r);
+				canvas.FillRectangle(r, brush);
 			}
 		}
 	}
@@ -236,28 +235,26 @@ bool CMeterBar::Draw(Graphics& graphics)
 				if (m_Border > 0)
 				{
 					Rect r2(x + m_W - size - 2 * m_Border, y, m_Border, m_H);
-					graphics.DrawImage(drawBitmap, r2, 0, 0, m_Border, m_H, UnitPixel);
+					canvas.DrawBitmap(drawBitmap, r2, Rect(0, 0, m_Border, m_H));
 					r2.X = x + m_W - m_Border;
-					graphics.DrawImage(drawBitmap, r2, m_W - m_Border, 0, m_Border, m_H, UnitPixel);
+					canvas.DrawBitmap(drawBitmap, r2, Rect(m_W - m_Border, 0, m_Border, m_H));
 				}
 
-				// Blit the image
 				Rect r(x + m_W - size - m_Border, y, size, m_H);
-				graphics.DrawImage(drawBitmap, r, m_W - size - m_Border, 0, size, m_H, UnitPixel);
+				canvas.DrawBitmap(drawBitmap, r, Rect(m_W - size - m_Border, 0, size, m_H));
 			}
 			else
 			{
 				if (m_Border > 0)
 				{
 					Rect r2(x, y, m_Border, m_H);
-					graphics.DrawImage(drawBitmap, r2, 0, 0, m_Border, m_H, UnitPixel);
+					canvas.DrawBitmap(drawBitmap, r2, Rect(0, 0, m_Border, m_H));
 					r2.X = x + size + m_Border;
-					graphics.DrawImage(drawBitmap, r2, m_W - m_Border, 0, m_Border, m_H, UnitPixel);
+					canvas.DrawBitmap(drawBitmap, r2, Rect(m_W - m_Border, 0, m_Border, m_H));
 				}
 
-				// Blit the image
 				Rect r(x + m_Border, y, size, m_H);
-				graphics.DrawImage(drawBitmap, r, m_Border, 0, size, m_H, UnitPixel);
+				canvas.DrawBitmap(drawBitmap, r, Rect(m_Border, 0, size, m_H));
 			}
 		}
 		else
@@ -266,12 +263,12 @@ bool CMeterBar::Draw(Graphics& graphics)
 			if (m_Flip)
 			{
 				Rect r(x + m_W - size, y, size, m_H);
-				graphics.FillRectangle(&brush, r);
+				canvas.FillRectangle(r, brush);
 			}
 			else
 			{
 				Rect r(x, y, size, m_H);
-				graphics.FillRectangle(&brush, r);
+				canvas.FillRectangle(r, brush);
 			}
 		}
 	}

Library/MeterBar.h
@@ -32,7 +32,7 @@ public:
 
 	virtual void Initialize();
 	virtual bool Update();
-	virtual bool Draw(Gdiplus::Graphics& graphics);
+	virtual bool Draw(Gfx::Canvas& canvas);
 
 protected:
 	virtual void ReadOptions(CConfigParser& parser, const WCHAR* section);

Library/MeterBitmap.cpp
@@ -22,6 +22,7 @@
 #include "Error.h"
 #include "Rainmeter.h"
 #include "System.h"
+#include "../Common/Gfx/Canvas.h"
 
 using namespace Gdiplus;
 
@@ -270,9 +271,9 @@ bool CMeterBitmap::HasActiveTransition()
 ** Draws the meter on the double buffer
 **
 */
-bool CMeterBitmap::Draw(Graphics& graphics)
+bool CMeterBitmap::Draw(Gfx::Canvas& canvas)
 {
-	if (!CMeter::Draw(graphics)) return false;
+	if (!CMeter::Draw(canvas)) return false;
 
 	int newY, newX;
 
@@ -336,8 +337,6 @@ bool CMeterBitmap::Draw(Graphics& graphics)
 		{
 			offset = offset - (m_W + m_Separation);
 
-			Rect r(x + offset, y, m_W, m_H);
-
 			int realFrames = (m_FrameCount / (m_TransitionFrameCount + 1));
 			int frame = (value % realFrames) * (m_TransitionFrameCount + 1);
 
@@ -377,7 +376,7 @@ bool CMeterBitmap::Draw(Graphics& graphics)
 				newY = 0;
 			}
 
-			graphics.DrawImage(bitmap, r, newX, newY, m_W, m_H, UnitPixel);
+			canvas.DrawBitmap(bitmap, Rect(x + offset, y, m_W, m_H), Rect(newX, newY, m_W, m_H));
 			if (m_FrameCount == 1)
 			{
 				value /= 2;
@@ -446,9 +445,7 @@ bool CMeterBitmap::Draw(Graphics& graphics)
 			newY = 0;
 		}
 
-		// Blit the image
-		Rect r(x, y, m_W, m_H);
-		graphics.DrawImage(bitmap, r, newX, newY, m_W, m_H, UnitPixel);
+		canvas.DrawBitmap(bitmap, Rect(x, y, m_W, m_H), Rect(newX, newY, m_W, m_H));
 	}
 
 	return true;

Library/MeterBitmap.h
@@ -34,7 +34,7 @@ public:
 
 	virtual void Initialize();
 	virtual bool Update();
-	virtual bool Draw(Gdiplus::Graphics& graphics);
+	virtual bool Draw(Gfx::Canvas& canvas);
 	virtual bool HasActiveTransition();
 
 protected:

Library/MeterButton.cpp
@@ -21,6 +21,7 @@
 #include "Measure.h"
 #include "Rainmeter.h"
 #include "Error.h"
+#include "../Common/Gfx/Canvas.h"
 
 extern CRainmeter* Rainmeter;
 
@@ -181,17 +182,21 @@ bool CMeterButton::Update()
 ** Draws the meter on the double buffer
 **
 */
-bool CMeterButton::Draw(Graphics& graphics)
+bool CMeterButton::Draw(Gfx::Canvas& canvas)
 {
-	if (!CMeter::Draw(graphics)) return false;
+	if (!CMeter::Draw(canvas)) return false;
 
 	if (m_Bitmaps[m_State] == NULL) return false;	// Unable to continue
+
+	Gdiplus::Graphics& graphics = canvas.BeginGdiplusContext();
 
 	int x = GetX();
 	int y = GetY();
 
 	// Blit the image
 	graphics.DrawCachedBitmap(m_Bitmaps[m_State], x, y);
+
+	canvas.EndGdiplusContext();
 
 	return true;
 }

commit cfd525c2fc84ac0d479a4b8f6c0d49d3ab20c23e

Fixed AnchorX/Y issue (caused by ef1cf223) http://rainmeter.net/forum/viewtopic.php?f=14&t=16798

Library/MeterWindow.cpp
@@ -2818,18 +2818,19 @@ void MeterWindow::UpdateWindow(int alpha, bool reset, bool canvasBeginDrawCalled
 	}
 
 	BLENDFUNCTION blendPixelFunction = {AC_SRC_OVER, 0, alpha, AC_SRC_ALPHA};
+	POINT ptWindowScreenPosition = {m_ScreenX, m_ScreenY};
 	POINT ptSrc = {0, 0};
 	SIZE szWindow = {m_Canvas->GetW(), m_Canvas->GetH()};
 
 	if (!canvasBeginDrawCalled) m_Canvas->BeginDraw();
 
 	HDC dcMemory = m_Canvas->GetDC();
-	if (!UpdateLayeredWindow(m_Window, nullptr, nullptr, &szWindow, dcMemory, &ptSrc, 0, &blendPixelFunction, ULW_ALPHA))
+	if (!UpdateLayeredWindow(m_Window, nullptr, &ptWindowScreenPosition, &szWindow, dcMemory, &ptSrc, 0, &blendPixelFunction, ULW_ALPHA))
 	{
 		// Retry after resetting WS_EX_LAYERED flag.
 		RemoveWindowExStyle(WS_EX_LAYERED);
 		AddWindowExStyle(WS_EX_LAYERED);
-		UpdateLayeredWindow(m_Window, nullptr, nullptr, &szWindow, dcMemory, &ptSrc, 0, &blendPixelFunction, ULW_ALPHA);
+		UpdateLayeredWindow(m_Window, nullptr, &ptWindowScreenPosition, &szWindow, dcMemory, &ptSrc, 0, &blendPixelFunction, ULW_ALPHA);
 	}
 	m_Canvas->ReleaseDC(dcMemory);
 