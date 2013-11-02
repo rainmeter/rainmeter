99e2b69ff81235d9ddf542acb590cff8ca0f9bdd
Birunthan Mohanathas <birunthan@mohanathas.com>
2013-11-02 11:58:25 +0200

    Manage: Disable resizable window frame

--- Library/DialogManage.cpp
+++ Library/DialogManage.cpp
@@ -86,7 +86,7 @@ void DialogManage::Open(int tab)
 	c_Dialog->ShowDialogWindow(
 		GetString(ID_STR_MANAGERAINMETER),
 		0, 0, 500, 322,
-		DS_CENTER | WS_POPUP | WS_MINIMIZEBOX | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME,
+		DS_CENTER | WS_POPUP | WS_MINIMIZEBOX | WS_CAPTION | WS_SYSMENU,
 		WS_EX_APPWINDOW | WS_EX_CONTROLPARENT | ((*GetString(ID_STR_ISRTL) == L'1') ? WS_EX_LAYOUTRTL : 0),
 		GetRainmeter().GetWindow());
 
