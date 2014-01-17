351fcbce5d3fed54a7318f37811e3e443eaf7bd1
Birunthan Mohanathas <birunthan@mohanathas.com>
2014-01-17 23:03:38 +0200

    DllExporter: Move under Build\ directory Also makes InputText use an architecture specific version of DllExporter.

---
+++ Build/DllExporter/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("© 2013 - Birunthan Mohanathas")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyProduct("Rainmeter")]
