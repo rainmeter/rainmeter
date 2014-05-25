6626c10351ea5be6c6ec570947cc76ad7b552bbf
jsmorley <jsmorley@gmail.com>
2014-05-25 10:15:33 -0400

    Updated versions to 3.1 (r2289)

--- Version.cs
+++ Version.cs
@@ -3,9 +3,9 @@ namespace Rainmeter
     public class Version
     {
 #if X64
-        public const string Informational = "3.1.0.2159 (64-bit)";
+        public const string Informational = "3.1.0.2289 (64-bit)";
 #else
-        public const string Informational = "3.1.0.2159 (32-bit)";
+        public const string Informational = "3.1.0.2289 (32-bit)";
 #endif
     }
 }
--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 3,1,0,2159
+#define FILEVER 3,1,0,2289
 #define PRODUCTVER FILEVER
-#define STRFILEVER "3.1.0.2159"
+#define STRFILEVER "3.1.0.2289"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"3.1.0"
 #define RAINMETER_VERSION ((3 * 1000000) + (1 * 1000) + 0)
-const int revision_number = 2159;
+const int revision_number = 2289;
 const bool revision_beta = true;
