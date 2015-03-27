e2afff7c727535eb26ce4622b91f7df3fdf9b357
Brian Ferguson <ferguson.brian@gmail.com>
2015-03-27 14:42:19 -0400

    Add new measure: Loop Options include: StartValue (defaults to 0) EndValue (defaults to 1) Increment (defaults to 0.01) LoopCount (defaults to 0, meaning endless), this limits the loop to a finite number of iterations (can be reset with command below).
    Also, a new command "Reset", that re-starts the loop.
    Usage: [!CommandMeasure LoopMeasureName Reset]

---
+++ Library/MeasureLoop.cpp
@@ -0,0 +1,111 @@
+/*
+Copyright (C) 2015 Brian Ferguson
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "StdAfx.h"
+#include "MeasureLoop.h"
+#include "Rainmeter.h"
+
+MeasureLoop::MeasureLoop(Skin* skin, const WCHAR* name) : Measure(skin, name),
+	m_StartValue(0.0f),
+	m_EndValue(1.0f),
+	m_Increment(0.01f),
+	m_IncSign(false),
+	m_LoopCount(0),
+	m_LoopCounter(0),
+	m_SkipFirst(true)
+{
+}
+
+MeasureLoop::~MeasureLoop()
+{
+}
+
+void MeasureLoop::ReadOptions(ConfigParser& parser, const WCHAR* section)
+{
+	double oldStart = m_StartValue, oldEnd = m_EndValue, oldInc = m_Increment;
+
+	Measure::ReadOptions(parser, section);
+
+	m_StartValue = parser.ReadFloat(section, L"StartValue", m_StartValue);
+	m_EndValue = parser.ReadFloat(section, L"EndValue", m_EndValue);
+	m_IncSign = !signbit(m_Increment = parser.ReadFloat(section, L"Increment", m_Increment));
+
+	m_LoopCount = parser.ReadInt(section, L"LoopCount", 0);
+
+	if (!m_Initialized || oldStart != m_StartValue || oldEnd != m_EndValue || oldInc != m_Increment)
+	{
+		Reset();
+
+		// Warn the user if the |m_EndValue| is never reached
+		if (abs(fmod((m_EndValue - m_StartValue), m_Increment)) != 0.0f)
+		{
+			LogWarningF(this, L"EndValue=%lf will never be reached", m_EndValue);
+		}
+	}
+}
+
+void MeasureLoop::UpdateValue()
+{
+	// Skip the first update of the loop, otherwise the |m_StartValue| gets skipped
+	if (m_SkipFirst)
+	{
+		m_SkipFirst = false;
+		return;
+	}
+
+	if (m_LoopCount == 0 || m_LoopCounter < m_LoopCount)
+	{
+		m_Value += m_Increment;
+
+		// |m_Value| is beyond |m_EndValue|, so start loop over
+		if ((m_IncSign && m_Value > m_EndValue) || (!m_IncSign && m_Value < m_EndValue))
+		{
+			m_Value = m_StartValue;
+			++m_LoopCounter;
+		}
+	}
+}
+
+void MeasureLoop::Reset()
+{
+	m_Value = m_StartValue;
+	m_LoopCounter = 0;
+	m_SkipFirst = true;
+
+	if (m_StartValue < m_EndValue)
+	{
+		m_MinValue = m_StartValue;
+		m_MaxValue = m_EndValue;
+	}
+	else
+	{
+		m_MinValue = m_EndValue;
+		m_MaxValue = m_StartValue;
+	}
+}
+
+void MeasureLoop::Command(const std::wstring& command)
+{
+	if (_wcsicmp(command.c_str(), L"Reset") == 0)
+	{
+		Reset();
+		return;
+	}
+
+	LogWarningF(this, L"!CommandMeasure: Not supported");
+}
---
+++ Library/MeasureLoop.h
@@ -0,0 +1,55 @@
+/*
+Copyright (C) 2015 Brian Ferguson
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef __MEASURELOOP_H__
+#define __MEASURELOOP_H__
+
+#include "Measure.h"
+
+class MeasureLoop : public Measure
+{
+public:
+	MeasureLoop(Skin* skin, const WCHAR* name);
+	virtual ~MeasureLoop();
+
+	MeasureLoop(const MeasureLoop& other) = delete;
+	MeasureLoop& operator=(MeasureLoop other) = delete;
+
+	virtual void Command(const std::wstring& command);
+
+	virtual UINT GetTypeID() { return TypeID<MeasureLoop>(); }
+
+protected:
+	virtual void ReadOptions(ConfigParser& parser, const WCHAR* section);
+	virtual void UpdateValue();
+
+private:
+	void Reset();
+
+	double m_StartValue;
+	double m_EndValue;
+	double m_Increment;
+	bool m_IncSign;
+
+	int m_LoopCount;
+	int m_LoopCounter;
+
+	bool m_SkipFirst;
+};
+
+#endif
