rainmeter/rainmeter

commit 553e5a261079008e6636a5f128edff77ad3f77ae

Use UTF-16 PCRE for IfActions

Library/IfActions.cpp
@@ -267,9 +267,9 @@ void IfActions::DoIfActions(Measure& measure, double value)
 			const char* error;
 			int errorOffset;
 
-			pcre* re = pcre_compile(
-				StringUtil::NarrowUTF8(item.value).c_str(),
-				PCRE_UTF8,
+			pcre16* re = pcre16_compile(
+				(PCRE_SPTR16)item.value.c_str(),
+				PCRE_UTF16,
 				&error,
 				&errorOffset,
 				nullptr);
@@ -294,14 +294,13 @@ void IfActions::DoIfActions(Measure& measure, double value)
 			{
 				item.parseError = false;
 
-				std::string utf8str = StringUtil::NarrowUTF8(measure.GetStringValue());
+				const std::wstring& str = measure.GetStringValue();
 				int ovector[300];
-
-				int rc = pcre_exec(
+				int rc = pcre16_exec(
 					re,
 					nullptr,
-					utf8str.c_str(),
-					(int)utf8str.length(),
+					(PCRE_SPTR16)str.c_str(),
+					(int)str.length(),
 					0,
 					0,
 					ovector,

commit 9db4d858c4a3dd3fa8405e55c30a8388b75bdd4c

IfActions: Fixed issue of nullptr being sent to PCRE in some cases introduced in 553e5a2

Library/IfActions.cpp
@@ -294,13 +294,14 @@ void IfActions::DoIfActions(Measure& measure, double value)
 			{
 				item.parseError = false;
 
-				const std::wstring& str = measure.GetStringValue();
+				const WCHAR* str = measure.GetStringValue();
+				int strLen = str ? wcslen(str) : 0;
 				int ovector[300];
 				int rc = pcre16_exec(
 					re,
 					nullptr,
-					(PCRE_SPTR16)str.c_str(),
-					(int)str.length(),
+					(PCRE_SPTR16)str,
+					(int)strLen,
 					0,
 					0,
 					ovector,