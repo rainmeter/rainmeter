8472b42ef9c4671c3521d55cf1814e6e80afebc9
brianferguson <ferguson.brian@gmail.com>
2017-03-06 21:52:12 -0500

    PluginCoreTemp: Cosmetics

--- Plugins/PluginCoreTemp/CoreTempProxy.h
+++ Plugins/PluginCoreTemp/CoreTempProxy.h
@@ -10,31 +10,31 @@ public:
 	virtual ~CoreTempProxy(void);
 	
 	UINT GetCoreLoad(int Index) const;
-    UINT GetTjMax(int Index) const;
-    UINT GetCoreCount() const;
-    UINT GetCPUCount() const;
-    float GetTemp(int Index) const;
-    float GetVID() const;
-    float GetCPUSpeed() const;
-    float GetFSBSpeed() const;
-    float GetMultiplier() const;
-    LPCSTR GetCPUName() const;
-    bool IsFahrenheit() const;
-    bool IsDistanceToTjMax() const;
-    bool IsTdpSupported() const;
-    bool IsPowerSupported() const;
+	UINT GetTjMax(int Index) const;
+	UINT GetCoreCount() const;
+	UINT GetCPUCount() const;
+	float GetTemp(int Index) const;
+	float GetVID() const;
+	float GetCPUSpeed() const;
+	float GetFSBSpeed() const;
+	float GetMultiplier() const;
+	LPCSTR GetCPUName() const;
+	bool IsFahrenheit() const;
+	bool IsDistanceToTjMax() const;
+	bool IsTdpSupported() const;
+	bool IsPowerSupported() const;
 	UINT GetStructureVersion() const;
-    UINT GetTdp(int Index) const;
-    float GetPower(int Index) const;
-    float GetMultiplier(int Index) const;
+	UINT GetTdp(int Index) const;
+	float GetPower(int Index) const;
+	float GetMultiplier(int Index) const;
 
-    const CoreTempSharedDataEx &GetDataStruct() const;
+	const CoreTempSharedDataEx &GetDataStruct() const;
 
 	bool GetData();
 	DWORD GetDllError() const { return GetLastError(); }
 	LPCWSTR GetErrorMessage();
-private:
 
+private:
 	CSharedMemClient m_SharedMem;
 	CoreTempSharedDataEx m_pCoreTempData;
 	WCHAR m_ErrorMessage[100];
