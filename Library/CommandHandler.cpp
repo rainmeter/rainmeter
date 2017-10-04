rainmeter/rainmeter

commit cf0f31ffd9815716c28d3d7f1763b30e6f082a33

Reverted 99a11be

Library/CommandHandler.cpp
@@ -8,6 +8,7 @@
 #include "StdAfx.h"
 #include "../Common/PathUtil.h"
 #include "CommandHandler.h"
+#include "ConfigParser.h"
 #include "DialogAbout.h"
 #include "DialogManage.h"
 #include "Measure.h"
@@ -256,7 +257,7 @@ void CommandHandler::ExecuteCommand(const WCHAR* command, Skin* skin, bool multi
 			if (pos)
 			{
 				bang.assign(command, 0, pos - command);
-				args = ParseString(pos + 1);
+				args = ParseString(pos + 1, skin ? &skin->GetParser() : nullptr);
 			}
 			else
 			{
@@ -296,9 +297,10 @@ void CommandHandler::ExecuteCommand(const WCHAR* command, Skin* skin, bool multi
 					start = bangs.find_first_not_of(L" \t\r\n", start + 1, 4);
 
 					const WCHAR* newCommand = bangs.c_str() + start;
+
 					if (skin && _wcsnicmp(newCommand, L"!Delay ", wcslen(L"!Delay ")) == 0)
 					{
-						auto args = ParseString(newCommand + wcslen(L"!Delay "));
+						auto args = ParseString(newCommand + wcslen(L"!Delay "), &skin->GetParser());
 						if (args.size() == 1)
 						{
 							auto delay = ConfigParser::ParseUInt(args[0].c_str(), 0);
@@ -357,6 +359,7 @@ void CommandHandler::ExecuteCommand(const WCHAR* command, Skin* skin, bool multi
 
 					if (skin)
 					{
+						skin->GetParser().ReplaceMeasures(sound);
 						skin->MakePathAbsolute(sound);
 					}
 
@@ -372,7 +375,12 @@ void CommandHandler::ExecuteCommand(const WCHAR* command, Skin* skin, bool multi
 		}
 
 		// Run command
-		RunCommand(command);
+		std::wstring tmpSz = command;
+		if (skin)
+		{
+			skin->GetParser().ReplaceMeasures(tmpSz);
+		}
+		RunCommand(tmpSz);
 	}
 }
 
@@ -483,7 +491,7 @@ void CommandHandler::RunFile(const WCHAR* file, const WCHAR* args)
 ** Splits strings into parts.
 **
 */
-std::vector<std::wstring> CommandHandler::ParseString(const WCHAR* str)
+std::vector<std::wstring> CommandHandler::ParseString(const WCHAR* str, ConfigParser* parser)
 {
 	std::vector<std::wstring> result;
 

Library/CommandHandler.h
@@ -12,6 +12,7 @@
 #include <string>
 #include <vector>
 
+class ConfigParser;
 class Skin;
 
 enum class Bang
@@ -117,7 +118,7 @@ public:
 	static void RunCommand(std::wstring command);
 	static void RunFile(const WCHAR* file, const WCHAR* args = nullptr);
 
-	static std::vector<std::wstring> ParseString(const WCHAR* str);
+	static std::vector<std::wstring> ParseString(const WCHAR* str, ConfigParser* parser = nullptr);
 
 	static void DoActivateSkinBang(std::vector<std::wstring>& args, Skin* skin);
 	static void DoDeactivateSkinBang(std::vector<std::wstring>& args, Skin* skin);

Library/Rainmeter.cpp
@@ -1282,12 +1282,7 @@ void Rainmeter::ExecuteBang(const WCHAR* bang, std::vector<std::wstring>& args, 
 */
 void Rainmeter::ExecuteCommand(const WCHAR* command, Skin* skin, bool multi)
 {
-	std::wstring tmpSz = command;
-	if (skin)
-	{
-		skin->GetParser().ReplaceMeasures(tmpSz);
-	}
-	m_CommandHandler.ExecuteCommand(tmpSz.c_str(), skin, multi);
+	m_CommandHandler.ExecuteCommand(command, skin, multi);
 }
 
 /*

commit eaa604aca309168e39d1583e488d8b025f9bb985

Fixed regression caused by cf0f31f

Library/CommandHandler.cpp
@@ -297,7 +297,6 @@ void CommandHandler::ExecuteCommand(const WCHAR* command, Skin* skin, bool multi
 					start = bangs.find_first_not_of(L" \t\r\n", start + 1, 4);
 
 					const WCHAR* newCommand = bangs.c_str() + start;
-
 					if (skin && _wcsnicmp(newCommand, L"!Delay ", wcslen(L"!Delay ")) == 0)
 					{
 						auto args = ParseString(newCommand + wcslen(L"!Delay "), &skin->GetParser());
@@ -532,6 +531,11 @@ std::vector<std::wstring> CommandHandler::ParseString(const WCHAR* str, ConfigPa
 					}
 				}
 				while (pos != std::wstring::npos);
+			}
+
+			if (parser)
+			{
+				parser->ReplaceMeasures(string);
 			}
 
 			result.push_back(string);