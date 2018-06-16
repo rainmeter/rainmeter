b55233204304d961e5977cbaa919bb9d364e8779
brianferguson <ferguson.brian@gmail.com>
2018-06-16 00:29:39 -0400

    FileUtil: Fixed encoding and line endings

--- Common/FileUtil.cpp
+++ Common/FileUtil.cpp
@@ -1,4 +1,4 @@
-﻿/* Copyright (C) 2015 Rainmeter Project Developers
+/* Copyright (C) 2015 Rainmeter Project Developers
  *
  * This Source Code Form is subject to the terms of the GNU General Public
  * License; either version 2 of the License, or (at your option) any later
