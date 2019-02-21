0a3dfa35357270512ec4a3c722674b67bff541d6
SnGmng <38666407+SnGmng@users.noreply.github.com>
2019-02-21 08:41:11 +0100

    Compatibility with the Nahimic audio driver (#195)
    * Compatibility with the Nahimic audio driver
    
    The plugin doesnt work on PCs that use the Nahimic audio driver, this commit fixes this issue.
    The issue is caused by Nahimic (the audio driver of MSI) because it is hooked into GetMixFormat and returns a wrong WAVEFORMATEX 
    see: https://social.msdn.microsoft.com/Forums/windowsdesktop/en-US/bd8cd9f2-974f-4a9f-8e9c-e83001819942/iaudioclient-initialize-failure
    
    * nahimic compatiblity while maintaining 7.1 support

--- Plugins/PluginAudioLevel/PluginAudioLevel.cpp
+++ Plugins/PluginAudioLevel/PluginAudioLevel.cpp
@@ -1170,7 +1170,18 @@ HRESULT	Measure::DeviceInit ()
 		hnsRequestedDuration, 0, m_wfx, NULL);
 	if (hr != S_OK)
 	{
-		RmLog(LOG_WARNING, L"Failed to initialize audio client.");
+		// initialization failed, try to use stereo waveformat
+		m_wfx->nChannels = 2;
+		m_wfx->nBlockAlign = (2 * m_wfx->wBitsPerSample) / 8;
+		m_wfx->nAvgBytesPerSec = m_wfx->nSamplesPerSec * m_wfx->nBlockAlign;
+
+		hr = m_clAudio->Initialize(AUDCLNT_SHAREMODE_SHARED, m_port == PORT_OUTPUT ? AUDCLNT_STREAMFLAGS_LOOPBACK : 0,
+			hnsRequestedDuration, 0, m_wfx, NULL);
+		if (hr != S_OK) 
+		{
+			// stereo waveformat didnt work either, throw an error
+			RmLog(LOG_WARNING, L"Failed to initialize audio client...");
+		}
 	}
 	EXIT_ON_ERROR(hr);
 
