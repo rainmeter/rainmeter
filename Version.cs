7bae52d181ed9be220e099a79713d8826dfe8524
brianferguson <ferguson.brian@gmail.com>
2019-11-05 21:46:49 -0700

    Finished updating to 4.4

--- Version.cs
+++ Version.cs
@@ -3,9 +3,9 @@ namespace Rainmeter
     public class Version
     {
 #if X64
-        public const string Informational = "4.3.0.3111 (64-bit)";
+        public const string Informational = "4.4.0.3321 (64-bit)";
 #else
-        public const string Informational = "4.3.0.3111 (32-bit)";
+        public const string Informational = "4.4.0.3321 (32-bit)";
 #endif
     }
 }
--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 4,3,0,3111
+#define FILEVER 4,4,0,3321
 #define PRODUCTVER FILEVER
-#define STRFILEVER "4.3.0.3111"
+#define STRFILEVER "4.4.0.3321"
 #define STRPRODUCTVER STRFILEVER
-#define APPVERSION L"4.3.0"
-#define RAINMETER_VERSION ((4 * 1000000) + (3 * 1000) + 0)
-const int revision_number = 3111;
+#define APPVERSION L"4.4.0"
+#define RAINMETER_VERSION ((4 * 1000000) + (4 * 1000) + 0)
+const int revision_number = 3321;
 const bool revision_beta = true;
