6eb1c31b95e7ee6906a650d15e6de52f53fb5563
jsmorley <jsmorley@gmail.com>
2019-11-27 17:12:58 -0500

    Some cosmetic changes to project files. https://forum.rainmeter.net/viewtopic.php?p=168498#p168498

--- Version.h
+++ Version.h
@@ -1,9 +1,11 @@
 #pragma once
-#define FILEVER 4,4,0,3321
+#define FILEVER 4,4,0,3338
 #define PRODUCTVER FILEVER
-#define STRFILEVER "4.4.0.3321"
+#define STRFILEVER "4.4.0.3338"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"4.4.0"
 #define RAINMETER_VERSION ((4 * 1000000) + (4 * 1000) + 0)
-const int revision_number = 3321;
+#define COMMIT_HASH L"ab72019a"
+#define BUILD_TIME L"2019-11-27 17:01:26"
+const int revision_number = 3338;
 const bool revision_beta = true;
