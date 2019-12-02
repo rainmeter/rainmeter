e7777fcfe5a5065aa49948c3dda2e35d61205cd0
brianferguson <ferguson.brian@gmail.com>
2019-12-02 01:24:18 -0700

    Partially revert 6eb1c31

--- Version.h
+++ Version.h
@@ -1,11 +1,9 @@
 #pragma once
-#define FILEVER 4,4,0,3338
+#define FILEVER 4,4,0,3321
 #define PRODUCTVER FILEVER
-#define STRFILEVER "4.4.0.3338"
+#define STRFILEVER "4.4.0.3321"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"4.4.0"
 #define RAINMETER_VERSION ((4 * 1000000) + (4 * 1000) + 0)
-#define COMMIT_HASH L"ab72019a"
-#define BUILD_TIME L"2019-11-27 17:01:26"
-const int revision_number = 3338;
+const int revision_number = 3321;
 const bool revision_beta = true;
