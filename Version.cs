78c9c5be5d53a8525eaee24cadb1510d7a2a2f05
VRBOX\GateKeeper <nikhil@doublestrike.com>
2020-05-29 20:55:36 -0400

    Updated file versioning version.*

--- Version.cs
+++ Version.cs
@@ -3,9 +3,9 @@ namespace Rainmeter
     public class Version
     {
 #if X64
-        public const string Informational = "4.4.0.3321 (64-bit)";
+        public const string Informational = "4.4.0.3351 (64-bit)";
 #else
-        public const string Informational = "4.4.0.3321 (32-bit)";
+        public const string Informational = "4.4.0.3351 (32-bit)";
 #endif
     }
 }
--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 4,4,0,3321
+#define FILEVER 4,4,0,3351
 #define PRODUCTVER FILEVER
-#define STRFILEVER "4.4.0.3321"
+#define STRFILEVER "4.4.0.3351"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"4.4.0"
 #define RAINMETER_VERSION ((4 * 1000000) + (4 * 1000) + 0)
-const int revision_number = 3321;
+const int revision_number = 3351;
 const bool revision_beta = true;
