6ad250fbbf071fe500d8a30aba0b01a3aa0c2401
Brian <ferguson.brian@gmail.com>
2020-06-21 02:08:26 -0600

    Bangs: Add support for WindowX/Y modifiers for the !Move bang

--- Library/Skin.cpp
+++ Library/Skin.cpp
@@ -978,9 +978,15 @@ void Skin::DoBang(Bang bang, const std::vector<std::wstring>& args)
 
 	case Bang::Move:
 		{
-			int x = m_Parser.ParseInt(args[0].c_str(), 0);
-			int y = m_Parser.ParseInt(args[1].c_str(), 0);
-			MoveWindow(x, y);
+			m_WindowX = args[0];
+			m_WindowY = args[1];
+			WindowToScreen();
+			MoveWindow(m_ScreenX, m_ScreenY);
+
+			if (GetRainmeter().GetDebug())
+			{
+				LogDebugF(this, L"!Move \"%s\" \"%s\" (%i, %i)", args[0].c_str(), args[1].c_str(), m_ScreenX, m_ScreenY);
+			}
 		}
 		break;
 
