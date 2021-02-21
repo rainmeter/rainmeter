d776efa2c5d40894ae78e16640a35e742866daae
VRBOX\GateKeeper <nikhil@doublestrike.com>
2021-02-21 16:21:20 -0500

    Fixed unintended merge markers

--- Version.h
+++ Version.h
@@ -1,19 +1,9 @@
 #pragma once
-<<<<<<< HEAD
-#define FILEVER 4,4,0,3351
+#define FILEVER 4,4,0,3433
 #define PRODUCTVER FILEVER
-#define STRFILEVER "4.4.0.3351"
+#define STRFILEVER "4.4.0.3433"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"4.4.0"
 #define RAINMETER_VERSION ((4 * 1000000) + (4 * 1000) + 0)
 const int revision_number = 3351;
-=======
-#define FILEVER 0,0,0,0
-#define PRODUCTVER FILEVER
-#define STRFILEVER "0.0.0.0"
-#define STRPRODUCTVER STRFILEVER
-#define APPVERSION L"0.0.0"
-#define RAINMETER_VERSION ((0 * 1000000) + (0 * 1000) + 0)
-const int revision_number = 0;
->>>>>>> ecdc3f6ed8ce68e7201563d659c23e4ece327704
 const bool revision_beta = true;
