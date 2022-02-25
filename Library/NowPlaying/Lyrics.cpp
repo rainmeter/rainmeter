55e4da294ca82bfa0fd70ef25f396b71f5b96024
Brian <ferguson.brian@gmail.com>
2022-02-24 23:36:55 -0700

    NowPlaying: Removed defunct LyricWikia from Lyrics option

--- Library/NowPlaying/Lyrics.cpp
+++ Library/NowPlaying/Lyrics.cpp
@@ -19,87 +19,9 @@ bool Lyrics::GetFromInternet(const std::wstring& artist, const std::wstring& tit
 	std::wstring encArtist = Internet::EncodeUrl(artist);
 	std::wstring encTitle = Internet::EncodeUrl(title);
 
-	bool found = GetFromWikia(encArtist, encTitle, out) ||
-				 GetFromLetras(encArtist, encTitle, out);
+	bool found = GetFromLetras(encArtist, encTitle, out);
 
 	return found;
-}
-
-/*
-** Download lyrics from LyricWiki.
-**
-*/
-bool Lyrics::GetFromWikia(const std::wstring& artist, const std::wstring& title, std::wstring& data)
-{
-	bool ret = false;
-	
-	std::wstring url = L"http://lyrics.wikia.com/api.php?func=getSong&fmt=json&artist=" + artist;
-	url += L"&song=";
-	url += title;
-
-	data = Internet::DownloadUrl(url, CP_UTF8);
-	if (!data.empty())
-	{
-		// First we get the URL to the actual wiki page
-		std::wstring::size_type pos = data.find(L"http://");
-		if (pos != std::wstring::npos)
-		{
-			data.erase(0, pos);
-			pos = data.find_first_of(L'\'');
-			url.assign(data, 0, pos);
-
-			// Fetch the wiki page
-			data = Internet::DownloadUrl(url, CP_UTF8);
-			if (!data.empty())
-			{
-				pos = data.find(L"'lyricbox'");
-				pos = data.find(L"&#", pos);
-				if (pos != std::wstring::npos)
-				{
-					// Get and decode lyrics
-					data.erase(0, pos);
-					pos = data.find(L"<!");
-					data.resize(pos);
-					Internet::DecodeReferences(data);
-
-					pos = data.find(L"[...]");
-					if (pos != std::wstring::npos)
-					{
-						// Skip incomplete lyrics
-						return ret;
-					}
-
-					pos = data.find(L"<p>");
-					if (pos != std::wstring::npos)
-					{
-						// Skip unavailable lyrics
-						return ret;
-					}
-
-					while ((pos = data.find(L"<br />"), pos) != std::wstring::npos)
-					{
-						data.replace(pos, 6, L"\n");
-					}
-
-					pos = data.find(L"<div class='lyricsbreak'>");
-					data.resize(pos);
-
-					// Get rid of all HTML tags
-					std::wstring::size_type len = 0;
-					while ((pos = data.find_first_of(L'<'), pos) != std::wstring::npos)
-					{
-						len = data.find_first_of(L'>', pos);
-						len -= pos;
-						data.erase(pos, ++len);
-					}
-
-					ret = true;
-				}
-			}
-		}
-	}
-
-	return ret;
 }
 
 /*
