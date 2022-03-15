46bd2878c8d19952ce4f834f2d84b6ce09d12813
Brian <ferguson.brian@gmail.com>
2022-03-15 15:07:43 -0600

    About dialog > Plugin tab: Fix issue when gathering plugin information

--- Library/DialogPackage.cpp
+++ Library/DialogPackage.cpp
@@ -7,10 +7,11 @@
 
 #include "StdAfx.h"
 #include "DialogPackage.h"
-#include "../Common/StringUtil.h"
 #include "SkinInstaller.h"
 #include "DialogInstall.h"
 #include "resource.h"
+#include "../Common/FileUtil.h"
+#include "../Common/StringUtil.h"
 #include "../Version.h"
 
 #include "iowin32.h"
@@ -751,12 +752,9 @@ INT_PTR CALLBACK DialogPackage::SelectPluginDlgProc(HWND hWnd, UINT uMsg, WPARAM
 
 				bool x32 = LOWORD(wParam) == IDC_PACKAGESELECTPLUGIN_32BITBROWSE_BUTTON;
 
-				LOADED_IMAGE* loadedImage = ImageLoad(StringUtil::Narrow(buffer).c_str(), nullptr);
-				if (loadedImage)
+				WORD machine = 0U;
+				if (FileUtil::GetBinaryFileBitness(buffer, machine))
 				{
-					WORD machine = loadedImage->FileHeader->FileHeader.Machine;
-					ImageUnload(loadedImage);
-
 					if ((x32 && machine == IMAGE_FILE_MACHINE_I386) || (!x32 && machine == IMAGE_FILE_MACHINE_AMD64))
 					{
 						// Check if same name as other DLL
