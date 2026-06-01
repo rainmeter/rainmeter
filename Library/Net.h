4dffeef7e1d54d990d62a104ce2c4c5f732caba8
Biru Mohanathas <birunthan@mohanathas.com>
2026-06-01 16:34:49 +0300

    WebParser: Refactor file download into thread safe Net::DownloadTask

--- Library/Net.h
+++ Library/Net.h
@@ -36,6 +36,30 @@ protected:
 	std::atomic<bool> m_AbortRequested;
 };
 
+// Async task to download an URL to a file.
+class DownloadTask : public Task
+{
+public:
+	typedef void (* ResultCallback)(const Task*, void*, const std::wstring&, HRESULT result);
+
+	static DownloadTask* Create(void* requestor, std::wstring url, std::wstring path, ResultCallback resultCallback);
+
+private:
+	DownloadTask(void* requestor, std::wstring url, std::wstring path, ResultCallback resultCallback);
+	virtual ~DownloadTask() {}
+
+	void StartWorkOnWorkerThread() override;
+	void FinishWorkOnMainThread() override;
+
+	// Request
+	std::wstring m_Url;
+	std::wstring m_Path;
+	ResultCallback m_ResultCallback = nullptr;
+
+	// Result
+	HRESULT m_Result = E_FAIL;
+};
+
 // Async task to fetch an URL from the web.
 class FetchTask : public Task
 {
