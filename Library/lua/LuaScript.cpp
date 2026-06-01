rainmeter/rainmeter

commit b531f3fed0b3aa62eab5940a76fbd64049371075

Lua: Allow the Update function to return 2 values (number and string)
Note: Return order doesn't matter

Library/lua/LuaScript.cpp
@@ -186,7 +186,6 @@ void LuaScript::RunFunction(const char* funcName)
 int LuaScript::RunFunctionWithReturn(const char* funcName, double& numValue, std::wstring& strValue)
 {
 	auto L = GetState();
-	int type = LUA_TNIL;
 
 	if (IsInitialized())
 	{
@@ -196,32 +195,57 @@ int LuaScript::RunFunctionWithReturn(const char* funcName, double& numValue, std
 		// Push the function onto the stack
 		lua_getfield(L, -1, funcName);
 
-		if (lua_pcall(L, 0, 1, 0))
+		if (lua_pcall(L, 0, 2, 0))
 		{
 			LuaHelper::ReportErrors();
-			lua_pop(L, 1);
+			lua_pop(L, 2);
 		}
 		else
 		{
-			type = lua_type(L, -1);
-			if (type == LUA_TNUMBER)
-			{
-				numValue = lua_tonumber(L, -1);
-			}
-			else if (type == LUA_TSTRING)
-			{
-				size_t strLen = 0;
-				const char* str = lua_tolstring(L, -1, &strLen);
-				strValue = m_Unicode ?
-					StringUtil::WidenUTF8(str, (int)strLen) : StringUtil::Widen(str, (int)strLen);
-				numValue = strtod(str, nullptr);
-			}
+			bool hasNumberResult = false;
+			bool hasStringResult = false;
 
-			lua_pop(L, 2);
+			auto getReturnedValue = [&]() -> void
+			{
+				int type = lua_type(L, -1);
+				switch (type)
+				{
+					case LUA_TNUMBER:
+						numValue = lua_tonumber(L, -1);
+						hasNumberResult = true;
+						break;
+
+					case LUA_TSTRING:
+						if (!hasStringResult)
+						{
+							size_t strLen = 0;
+							const char* str = lua_tolstring(L, -1, &strLen);
+							strValue = m_Unicode ?
+								StringUtil::WidenUTF8(str, (int)strLen) : StringUtil::Widen(str, (int)strLen);
+
+							hasStringResult = true;
+							if (!hasNumberResult)
+							{
+								// Only convert the string value to number if number value has not been set
+								numValue = strtod(str, nullptr);
+								hasNumberResult = true;
+							}
+
+						}
+						break;
+				}
+				lua_pop(L, 1);
+			};
+
+			getReturnedValue();  // Get first returned value
+			getReturnedValue();  // Get second returned value
+
+			if (hasStringResult) return LUA_TSTRING;
+			if (hasNumberResult) return LUA_TNUMBER;
 		}
 	}
 
-	return type;
+	return LUA_TNIL;
 }
 
 /*

commit 8ab98bd8ecef05b77fc39a23c6bfe5cbacd971b5

Script: Fix memory corruption when the script Update/GetString functions throw an error
This was a regression in b531f3f. We were popping 1 item too many from
the Lua stack after the lua_pcall failed.

Library/lua/LuaScript.cpp
@@ -175,6 +175,7 @@ void LuaScript::RunFunction(const char* funcName)
 			LuaHelper::ReportErrors();
 		}
 
+		// Pop the table
 		lua_pop(L, 1);
 	}
 }
@@ -198,7 +199,9 @@ int LuaScript::RunFunctionWithReturn(const char* funcName, double& numValue, std
 		if (lua_pcall(L, 0, 2, 0))
 		{
 			LuaHelper::ReportErrors();
-			lua_pop(L, 2);
+
+			// Pop the table
+			lua_pop(L, 1);
 		}
 		else
 		{
@@ -234,13 +237,16 @@ int LuaScript::RunFunctionWithReturn(const char* funcName, double& numValue, std
 						}
 						break;
 				}
-				lua_pop(L, 1);  // Remove the returned value from the stack
+
+				// Pop the value
+				lua_pop(L, 1);
 			};
 
 			getReturnedValue();  // Get first returned value
 			getReturnedValue();  // Get second returned value
 
-			lua_settop(L, 0);  // Remove any remaining items from the stack
+			// Pop the table
+			lua_pop(L, 1);
 
 			if (hasStringResult) return LUA_TSTRING;
 			if (hasNumberResult) return LUA_TNUMBER;