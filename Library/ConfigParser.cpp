rainmeter/rainmeter

commit 945e649a4984c897783fd32d7d3092b9ba018271

Library: Switch to much faster ankerl::unordered_dense::map/set

Library/ConfigParser.cpp
@@ -168,7 +168,7 @@ int GetMonitorRectValue(const RECT& rect, MonitorComponent component)
 
 }  // namespace
 
-std::unordered_map<ConfigParser::VariableType, WCHAR> ConfigParser::c_VariableMap;
+ankerl::unordered_dense::map<ConfigParser::VariableType, WCHAR> ConfigParser::c_VariableMap;
 
 ConfigParser::ConfigParser() :
 	m_LastReplaced(false),
@@ -300,7 +300,7 @@ bool ConfigParser::GetVariable(const std::wstring& strVariable, std::wstring& st
 	const std::wstring strTmp = StrToUpper(strVariable);
 
 	// #1: Built-in variables
-	std::unordered_map<std::wstring, std::wstring>::const_iterator iter = m_BuiltInVariables.find(strTmp);
+	auto iter = m_BuiltInVariables.find(strTmp);
 	if (iter != m_BuiltInVariables.end())
 	{
 		strValue = (*iter).second;
@@ -329,7 +329,7 @@ const std::wstring* ConfigParser::GetVariableOriginalName(const std::wstring& st
 	const std::wstring strTmp = StrToUpper(strVariable);
 
 	// User-defined variables
-	std::unordered_map<std::wstring, std::wstring>::const_iterator iter = m_OriginalVariableNames.find(strTmp);
+	auto iter = m_OriginalVariableNames.find(strTmp);
 	if (iter != m_OriginalVariableNames.end())
 	{
 		return &(*iter).second;
@@ -1325,7 +1325,7 @@ void ConfigParser::AddMeasure(Measure* pMeasure)
 
 Measure* ConfigParser::GetMeasure(const std::wstring& name)
 {
-	std::unordered_map<std::wstring, Measure*>::const_iterator iter = m_Measures.find(StrToUpper(name));
+	auto iter = m_Measures.find(StrToUpper(name));
 	if (iter != m_Measures.end())
 	{
 		return (*iter).second;
@@ -1643,7 +1643,7 @@ void ConfigParser::ReadIniFile(const std::wstring& iniFile, LPCTSTR skinSection,
 
 	// Get all the sections (i.e. different meters)
 	std::list<std::wstring> sections;
-	std::unordered_set<std::wstring> unique;
+	ankerl::unordered_dense::set<std::wstring> unique;
 	std::wstring key, value;  // buffer
 
 	DWORD itemsSize = MAX_LINE_LENGTH;
@@ -1877,7 +1877,7 @@ void ConfigParser::DeleteValue(const std::wstring& strSection, const std::wstrin
 	strTmp += L'~';
 	strTmp += strKey;
 
-	std::unordered_map<std::wstring, std::wstring>::const_iterator iter = m_Values.find(StrToUpperC(strTmp));
+	auto iter = m_Values.find(StrToUpperC(strTmp));
 	if (iter != m_Values.end())
 	{
 		m_Values.erase(iter);
@@ -1896,6 +1896,6 @@ const std::wstring& ConfigParser::GetValue(const std::wstring& strSection, const
 	strTmp += L'~';
 	strTmp += strKey;
 
-	std::unordered_map<std::wstring, std::wstring>::const_iterator iter = m_Values.find(StrToUpperC(strTmp));
+	auto iter = m_Values.find(StrToUpperC(strTmp));
 	return (iter != m_Values.end()) ? (*iter).second : strDefault;
 }

Library/ConfigParser.h
@@ -14,8 +14,6 @@
 #include <windows.h>
 #include <string>
 #include <vector>
-#include <unordered_set>
-#include <unordered_map>
 #include <cstdint>
 #include <d2d1.h>
 
@@ -58,7 +56,7 @@ public:
 	void SetVariable(std::wstring strVariable, const std::wstring& strValue);
 	void SetBuiltInVariable(const std::wstring& strVariable, const std::wstring& strValue);
 
-	const std::unordered_map<std::wstring, std::wstring>& GetVariables() { return m_Variables; }
+	const ankerl::unordered_dense::map<std::wstring, std::wstring>& GetVariables() { return m_Variables; }
 	MonitorVariableMode GetMonitorVariableMode() const { return m_MonitorVariableMode; }
 	void SetMonitorVariableMode(MonitorVariableMode mode) { m_MonitorVariableMode = mode; }
 
@@ -127,7 +125,7 @@ private:
 	static std::wstring StrToUpper(const WCHAR* str) { std::wstring strTmp(str); StrToUpperC(strTmp); return strTmp; }
 	static std::wstring& StrToUpperC(std::wstring& str) { _wcsupr(&str[0]); return str; }
 
-	std::unordered_map<std::wstring, Measure*> m_Measures;
+	ankerl::unordered_dense::map<std::wstring, Measure*> m_Measures;
 
 	std::vector<std::wstring> m_StyleTemplate;
 
@@ -139,19 +137,19 @@ private:
 	std::wstring* m_CurrentSection;
 
 	std::list<std::wstring> m_Sections;		// Ordered section
-	std::unordered_map<std::wstring, std::wstring> m_Values;
+	ankerl::unordered_dense::map<std::wstring, std::wstring> m_Values;
 
-	std::unordered_set<std::wstring> m_FoundSections;
+	ankerl::unordered_dense::set<std::wstring> m_FoundSections;
 	std::list<std::wstring> m_ListVariables;
 	std::list<std::wstring>::const_iterator m_SectionInsertPos;
 
-	std::unordered_map<std::wstring, std::wstring> m_BuiltInVariables;
-	std::unordered_map<std::wstring, std::wstring> m_Variables;
-	std::unordered_map<std::wstring, std::wstring> m_OriginalVariableNames;
+	ankerl::unordered_dense::map<std::wstring, std::wstring> m_BuiltInVariables;
+	ankerl::unordered_dense::map<std::wstring, std::wstring> m_Variables;
+	ankerl::unordered_dense::map<std::wstring, std::wstring> m_OriginalVariableNames;
 
 	Skin* m_Skin;
 
-	static std::unordered_map<VariableType, WCHAR> c_VariableMap;
+	static ankerl::unordered_dense::map<VariableType, WCHAR> c_VariableMap;
 };
 
 #endif

Library/DialogInstall.cpp
@@ -990,7 +990,7 @@ UINT __stdcall DialogInstall::InstallThread(void* pParam)
 
 void DialogInstall::KeepVariables()
 {
-	auto getPairs = [](WCHAR* section, std::unordered_map<std::wstring, std::wstring>& var) -> void
+	auto getPairs = [](WCHAR* section, ankerl::unordered_dense::map<std::wstring, std::wstring>& var) -> void
 	{
 		std::wstring str, key, value;
 		for (LPCWSTR ptr = section; *ptr; ptr += str.length() + 1)
@@ -1015,8 +1015,8 @@ void DialogInstall::KeepVariables()
 		fromPath.append(file);
 		std::wstring toPath = g_Data.skinsPath + file;
 
-		std::unordered_map<std::wstring, std::wstring> fromVariables;
-		std::unordered_map<std::wstring, std::wstring> toVariables;
+		ankerl::unordered_dense::map<std::wstring, std::wstring> fromVariables;
+		ankerl::unordered_dense::map<std::wstring, std::wstring> toVariables;
 
 		if (_waccess_s(fromPath.c_str(), 0) != 0 || _waccess_s(toPath.c_str(), 0) != 0) continue;	// Both files need to exist
 

Library/Group.h
@@ -9,7 +9,6 @@
 #define __GROUP_H__
 
 #include <string>
-#include <unordered_set>
 
 class __declspec(novtable) Group
 {
@@ -22,7 +21,7 @@ public:
 
 	void InitializeGroup(const std::wstring& groups);
 
-	const std::unordered_set<std::wstring>& GetGroups() const { return m_Groups; }
+	const ankerl::unordered_dense::set<std::wstring>& GetGroups() const { return m_Groups; }
 
 	bool AddToGroup(const std::wstring& group);
 	bool BelongsToGroup(const std::wstring& group) const;
@@ -31,7 +30,7 @@ private:
 	std::wstring& CreateGroup(std::wstring& str) const;
 	std::wstring VerifyGroup(const std::wstring& str) const;
 
-	std::unordered_set<std::wstring> m_Groups;
+	ankerl::unordered_dense::set<std::wstring> m_Groups;
 	std::wstring m_OldGroups;
 
 };

Library/ImageCache.h
@@ -8,8 +8,6 @@
 #ifndef __IMAGECACHE_H__
 #define __IMAGECACHE_H__
 
-#include <unordered_map>
-#include <map>
 #include <string>
 #include <../Common/Gfx/D2DBitmap.h>
 #include "ImageOptions.h"
@@ -116,7 +114,7 @@ private:
 
 	void Remove(const ImageOptions& item);
 
-	std::unordered_map<ImageOptions, ImageCache*> m_CachePool;
+	ankerl::unordered_dense::map<ImageOptions, ImageCache*> m_CachePool;
 };
 
 // Convenience function.

Library/Language.cpp
@@ -113,7 +113,7 @@ bool Language::Load(const std::wstring& directory, const std::wstring& language)
 		return value;
 	};
 
-	std::unordered_map<UINT, const WCHAR*> strings;
+	ankerl::unordered_dense::map<UINT, const WCHAR*> strings;
 	size_t offset = 12;
 	while (offset < dataSize)
 	{

Library/Language.h
@@ -10,7 +10,6 @@
 
 #include <windows.h>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 class Language
@@ -47,7 +46,7 @@ private:
 	LCID m_LCID;
 	HANDLE m_FileMapping;
 	const BYTE* m_Data;
-	std::unordered_map<UINT, const WCHAR*> m_Strings;
+	ankerl::unordered_dense::map<UINT, const WCHAR*> m_Strings;
 	unsigned short m_ButtonWidth;
 	unsigned short m_LabelWidth;
 	bool m_IsRTL;

Library/Skin.cpp
@@ -772,7 +772,7 @@ void Skin::MoveSelectedWindow(int dx, int dy)
 	SavePositionIfAppropriate();
 }
 
-void Skin::SelectSkinsGroup(std::unordered_set<std::wstring> groups)
+void Skin::SelectSkinsGroup(const ankerl::unordered_dense::set<std::wstring>& groups)
 {
 	for (const auto& group : groups)
 	{
@@ -3022,7 +3022,7 @@ void Skin::UpdateRelativeMeters()
 {
 	if (!m_ResetRelativeMeters) return;
 
-	std::unordered_map<Meter*, Meter*> containers;
+	ankerl::unordered_dense::map<Meter*, Meter*> containers;
 	Meter* previousMeter = nullptr;
 
 	for (auto* meter : m_Meters)

commit 197850cbf4b0f831e3c08d319d116231f00e6e70

ConfigParser: Fix crash caused by unstable std::unordered_dense::map iterator reference
This was a regression in 945e649.

Library/ConfigParser.cpp
@@ -208,7 +208,7 @@ void ConfigParser::Initialize(const std::wstring& filename, Skin* skin, LPCTSTR 
 	m_LastValueDefined = false;
 	m_MonitorVariableMode = MonitorVariableMode::DEFAULT_LOGICAL;
 
-	m_CurrentSection = nullptr;
+	m_CurrentSection.clear();
 	m_SectionInsertPos = m_Sections.end();
 
 	// Set the built-in variables. Do this before the ini file is read so that the paths can be used with @include
@@ -251,8 +251,6 @@ void ConfigParser::SetBuiltInVariables(const std::wstring& filename, const std::
 	}
 
 	insertVariable(L"CRLF", L"\n");
-
-	m_CurrentSection = &(insertVariable(L"CURRENTSECTION", L"").first->second);	// shortcut
 
 	if (resourcePath)
 	{
@@ -304,6 +302,13 @@ bool ConfigParser::GetVariable(const std::wstring& strVariable, std::wstring& st
 	if (iter != m_BuiltInVariables.end())
 	{
 		strValue = (*iter).second;
+		return true;
+	}
+
+	static const std::wstring s_CurrentSectionStr = L"CURRENTSECTION";
+	if (strTmp == s_CurrentSectionStr)
+	{
+		strValue = m_CurrentSection;
 		return true;
 	}
 
@@ -890,12 +895,12 @@ bool ConfigParser::ParseVariables(std::wstring& str, const VariableType type, Me
 	// Since actions are parsed when executed, get the current active
 	// section in case the current section variable is used.
 	bool hasCurrentAction = false;
-	if (m_Skin && (m_CurrentSection->empty() || meter))
+	if (m_Skin && (m_CurrentSection.empty() || meter))
 	{
 		Section* section = m_Skin->GetCurrentActionSection();
 		if (section || meter)
 		{
-			m_CurrentSection->assign(meter ? meter->GetName() : section->GetName());
+			m_CurrentSection.assign(meter ? meter->GetName() : section->GetName());
 			hasCurrentAction = true;
 		}
 	}
@@ -926,11 +931,11 @@ bool ConfigParser::ParseVariables(std::wstring& str, const VariableType type, Me
 		// Restrict the number of variable replacements to a reseasonable amount
 		if (++counter >= maxReplacements)
 		{
-			LogErrorSF(m_Skin, m_CurrentSection->c_str(),
+			LogErrorSF(m_Skin, m_CurrentSection.c_str(),
 				L"Parsing Error: Maximum number of variable replacements reached (%llu) in string: %s", maxReplacements, str.c_str());
 			if (GetRainmeter().GetDebug())
 			{
-				LogDebugSF(m_Skin, m_CurrentSection->c_str(), L"Parsing Error: Result: %s", result.c_str());
+				LogDebugSF(m_Skin, m_CurrentSection.c_str(), L"Parsing Error: Result: %s", result.c_str());
 			}
 			break;
 		}
@@ -971,7 +976,7 @@ bool ConfigParser::ParseVariables(std::wstring& str, const VariableType type, Me
 			// Avoid self references
 			if (previousStart == start && _wcsicmp(original.c_str(), previousVariable.c_str()) == 0)
 			{
-				LogErrorSF(m_Skin, m_CurrentSection->c_str(),
+				LogErrorSF(m_Skin, m_CurrentSection.c_str(),
 					L"Cannot replace variable with itself: \"%s\"", original.c_str());
 				break;		// Break out of inner "start" loop and continue to the next nested variable
 			}
@@ -1140,18 +1145,18 @@ bool ConfigParser::ParseVariables(std::wstring& str, const VariableType type, Me
 	// Log the self reference warning(s)
 	if (!selfReferencedVariable.empty())
 	{
-		LogWarningSF(m_Skin, m_CurrentSection->c_str(), L"Warning: Potential self-referenced variable: %s", selfReferencedVariable.c_str());
+		LogWarningSF(m_Skin, m_CurrentSection.c_str(), L"Warning: Potential self-referenced variable: %s", selfReferencedVariable.c_str());
 		if (GetRainmeter().GetDebug())
 		{
-			LogDebugSF(m_Skin, m_CurrentSection->c_str(), L"Original string: %s", str.c_str());
-			LogDebugSF(m_Skin, m_CurrentSection->c_str(), L"Replaced string: %s", result.c_str());
+			LogDebugSF(m_Skin, m_CurrentSection.c_str(), L"Original string: %s", str.c_str());
+			LogDebugSF(m_Skin, m_CurrentSection.c_str(), L"Replaced string: %s", result.c_str());
 		}
 	}
 
 	// Reset the current section
 	if (hasCurrentAction)
 	{
-		m_CurrentSection->clear();
+		m_CurrentSection.clear();
 	}
 
 	str = result;
@@ -1273,7 +1278,7 @@ const std::wstring& ConfigParser::ReadString(LPCTSTR section, LPCTSTR key, LPCTS
 
 	if (!result.empty())
 	{
-		m_CurrentSection->assign(strSection);  // Set temporarily
+		m_CurrentSection.assign(strSection);  // Set temporarily
 		m_LastValueDefined = true;
 
 		if (result.size() >= 3)
@@ -1297,7 +1302,7 @@ const std::wstring& ConfigParser::ReadString(LPCTSTR section, LPCTSTR key, LPCTS
 				m_LastReplaced = true;
 			}
 		}
-		m_CurrentSection->clear();  // Reset
+		m_CurrentSection.clear();  // Reset
 	}
 
 	return result;

Library/ConfigParser.h
@@ -134,7 +134,7 @@ private:
 	bool m_LastValueDefined;
 	MonitorVariableMode m_MonitorVariableMode;
 
-	std::wstring* m_CurrentSection;
+	std::wstring m_CurrentSection;
 
 	std::list<std::wstring> m_Sections;		// Ordered section
 	ankerl::unordered_dense::map<std::wstring, std::wstring> m_Values;