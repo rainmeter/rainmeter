rainmeter/rainmeter

commit f799a942c99757eb84acec6ee98b166236a48607

Skin: Wait until selection is dismissed before saving skin options to .ini

Library/Skin.cpp
@@ -73,6 +73,7 @@ Skin::Skin(const std::wstring& folderPath, const std::wstring& file, const bool 
 	m_HostWindow(),
 	m_Window(),
 	m_SelectionOverlay(),
+	m_PendingWriteOptions(0),
 	m_SuspendResumeNotification(nullptr),
 	m_Mouse(this),
 	m_MouseOver(false),
@@ -824,15 +825,20 @@ void Skin::Deselect()
 {
 	if (!IsSelected()) return;
 
-	m_SelectionOverlay.reset();
-
 	// Reset the following options to their original state
 	SetWindowDraggable(m_OldWindowDraggable);
 	SetKeepOnScreen(m_OldKeepOnScreen);
 	SetClickThrough(m_OldClickThrough);
 	DialogManage::UpdateSelectedSkinOptions(this);
 
-	// Reset each meter's tooltip
+	m_SelectionOverlay.reset();
+
+	if (m_PendingWriteOptions != 0)
+	{
+		WriteOptions(m_PendingWriteOptions);
+		m_PendingWriteOptions = 0;
+	}
+
 	for (const auto& meter : m_Meters) meter->ResetToolTip();
 
 	Redraw();
@@ -2195,6 +2201,16 @@ void Skin::ReadOptions(ConfigParser& parser, LPCWSTR section, bool isDefault)
 */
 void Skin::WriteOptions(INT setting)
 {
+	if (IsSelected())
+	{
+		m_PendingWriteOptions |= setting;
+		if (setting != OPTION_ALL)
+		{
+			DialogManage::UpdateSkins(this);
+		}
+		return;
+	}
+
 	const WCHAR* iniFile = GetRainmeter().GetIniFile().c_str();
 
 	if (*iniFile)

Library/Skin.h
@@ -392,6 +392,9 @@ private:
 	HWND m_HostWindow;
 	HWND m_Window;
 	std::unique_ptr<SkinSelectionOverlay> m_SelectionOverlay;
+
+	int m_PendingWriteOptions;
+
 	HPOWERNOTIFY m_SuspendResumeNotification;
 
 	Mouse m_Mouse;

commit b9a4584d636ec32c87c91867c90b449a739ac933

Skin: Try fixing skins snapping back on nudge
This was a regression in f799a94.

Library/Skin.cpp
@@ -2201,6 +2201,11 @@ void Skin::ReadOptions(ConfigParser& parser, LPCWSTR section, bool isDefault)
 */
 void Skin::WriteOptions(INT setting)
 {
+	if (setting & OPTION_POSITION)
+	{
+		ComputeOptionValueFromPosition();
+	}
+
 	if (IsSelected())
 	{
 		m_PendingWriteOptions |= setting;
@@ -2234,9 +2239,6 @@ void Skin::WriteOptions(INT setting)
 
 		if (setting & OPTION_POSITION)
 		{
-			ComputeOptionValueFromPosition();
-
-			// If position needs to be save, do so.
 			if (m_SavePosition)
 			{
 				WritePrivateProfileString(section, L"WindowX", m_X.option.c_str(), iniFile);