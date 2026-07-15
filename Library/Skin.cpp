rainmeter/rainmeter

commit 2eae2d1e02a812c2eff680b173153e23cd573c6f

Skin: Convert between screen logical and physical positions with Windows APIs
Previously MonitorUtil was converting between logical and physical
coordinates in the screen coordinate space. This worked fine if the
point was within the monitor areas, but did not behave consistently with
DPI unaware Rainmeter when the point was outside the monitor bounds.

We now use the DpiUnawareScope trick to obtain the physical screen
position after moving a helper window by its logical screen position.

Library/Skin.cpp
@@ -647,11 +647,6 @@ POINT Skin::GetMouseMessageSkinPosition(UINT uMsg, LPARAM lParam) const
 	return PhysicalToLogical(pos);
 }
 
-POINT Skin::GetLogicalWindowPosition() const
-{
-	return MonitorUtil::GetMultiMonitorInfo().PhysicalToLogical({ m_X.pos, m_Y.pos });
-}
-
 int Skin::GetZoomedWindowW() const
 {
 	return (int)roundf((float)GetCurrentConfigW() * m_ZoomScale);
@@ -711,6 +706,23 @@ POINT Skin::PhysicalToRelativeLogical(POINT point) const
 	point.x -= m_X.pos;
 	point.y -= m_Y.pos;
 	return PhysicalToLogical(point);
+}
+
+POINT Skin::GetScreenLogicalPosition() const
+{
+	return PhysicalToScreenLogical({ m_X.pos, m_Y.pos });
+}
+
+POINT Skin::ScreenLogicalToPhysical(POINT point) const
+{
+	LogicalToPhysicalPointForPerMonitorDPI(m_Window, &point);
+	return point;
+}
+
+POINT Skin::PhysicalToScreenLogical(POINT point) const
+{
+	PhysicalToLogicalPointForPerMonitorDPI(m_Window, &point);
+	return point;
 }
 
 void Skin::RepositionAndResizeWindow()
@@ -2079,7 +2091,7 @@ void Skin::ComputeOptionValueFromPosition()
 		}
 	}
 
-	const POINT logicalPos = monitorsInfo.PhysicalToLogical({ m_X.pos, m_Y.pos });
+	const POINT logicalPos = GetScreenLogicalPosition();
 
 	const int monitorX = m_X.monitor.value_or(monitorsInfo.primary);
 	const auto& monitorRectX = monitorX == 0 ? monitorsInfo.logicalVirtualScreen : monitors[monitorX - 1].logicalScreen;

Library/ConfigParser.cpp
@@ -495,8 +495,8 @@ std::optional<std::wstring> ConfigParser::GetCurrentConfigVariable(std::wstring_
 
 	auto strParser = StringParser(variableStr);
 	if (!strParser.Consume(L"CURRENTCONFIG")) return std::nullopt;
-	if (strParser.ConsumeRest(L"X")) return fmt::to_wstring(m_Skin->GetLogicalWindowPosition().x);
-	if (strParser.ConsumeRest(L"Y")) return fmt::to_wstring(m_Skin->GetLogicalWindowPosition().y);
+	if (strParser.ConsumeRest(L"X")) return fmt::to_wstring(m_Skin->GetScreenLogicalPosition().x);
+	if (strParser.ConsumeRest(L"Y")) return fmt::to_wstring(m_Skin->GetScreenLogicalPosition().y);
 	if (strParser.ConsumeRest(L"WIDTH")) return fmt::to_wstring(m_Skin->GetCurrentConfigW());
 	if (strParser.ConsumeRest(L"HEIGHT")) return fmt::to_wstring(m_Skin->GetCurrentConfigH());
 	if (strParser.ConsumeRest(L"ZPOS")) return fmt::to_wstring((int)m_Skin->GetWindowZPosition());
@@ -512,8 +512,8 @@ std::optional<std::wstring> ConfigParser::GetDollarSkinVariable(std::wstring_vie
 	auto strParser = StringParser(variableStr);
 	if (!strParser.Consume(L"Skin:")) return std::nullopt;
 
-	if (strParser.ConsumeRest(L"X")) return fmt::to_wstring(m_Skin->GetLogicalWindowPosition().x);
-	if (strParser.ConsumeRest(L"Y")) return fmt::to_wstring(m_Skin->GetLogicalWindowPosition().y);
+	if (strParser.ConsumeRest(L"X")) return fmt::to_wstring(m_Skin->GetScreenLogicalPosition().x);
+	if (strParser.ConsumeRest(L"Y")) return fmt::to_wstring(m_Skin->GetScreenLogicalPosition().y);
 	if (strParser.ConsumeRest(L"W")) return fmt::to_wstring(m_Skin->GetCurrentConfigW());
 	if (strParser.ConsumeRest(L"H")) return fmt::to_wstring(m_Skin->GetCurrentConfigH());
 

Library/DialogManage.cpp
@@ -700,7 +700,7 @@ void DialogManage::TabSkins::SetControls()
 
 	if (m_SkinWindow)
 	{
-		const auto logicalPos = m_SkinWindow->GetLogicalWindowPosition();
+		const auto logicalPos = m_SkinWindow->GetScreenLogicalPosition();
 
 		SetWindowText(item, GetString(IDS_Unload));
 
@@ -1324,11 +1324,14 @@ INT_PTR DialogManage::TabSkins::OnCommand(WPARAM wParam, LPARAM lParam)
 			{
 				WCHAR buffer[32];
 				m_IgnoreUpdate = true;
-				int x = (GetWindowText((HWND)lParam, buffer, 32) > 0) ? _wtoi(buffer) : 0;
-				m_SkinWindow->MoveWindow(x, m_SkinWindow->GetLogicalWindowPosition().y);
 
-				const int newX = m_SkinWindow->GetLogicalWindowPosition().x;
-				if (x != newX)
+				auto logicalPos = m_SkinWindow->GetScreenLogicalPosition();
+				logicalPos.x = (GetWindowText((HWND)lParam, buffer, 32) > 0) ? _wtoi(buffer) : 0;
+				const auto physicalPos = m_SkinWindow->ScreenLogicalToPhysical(logicalPos);
+				m_SkinWindow->MoveWindow(physicalPos.x, physicalPos.y);
+
+				const int newX = m_SkinWindow->GetScreenLogicalPosition().x;
+				if (logicalPos.x != newX)
 				{
 					_itow_s(newX, buffer, 10);
 					Edit_SetText((HWND)lParam, buffer);
@@ -1344,11 +1347,14 @@ INT_PTR DialogManage::TabSkins::OnCommand(WPARAM wParam, LPARAM lParam)
 			{
 				WCHAR buffer[32];
 				m_IgnoreUpdate = true;
-				int y = (GetWindowText((HWND)lParam, buffer, 32) > 0) ? _wtoi(buffer) : 0;
-				m_SkinWindow->MoveWindow(m_SkinWindow->GetLogicalWindowPosition().x, y);
 
-				const int newY = m_SkinWindow->GetLogicalWindowPosition().y;
-				if (y != newY)
+				auto logicalPos = m_SkinWindow->GetScreenLogicalPosition();
+				logicalPos.y = (GetWindowText((HWND)lParam, buffer, 32) > 0) ? _wtoi(buffer) : 0;
+				const auto physicalPos = m_SkinWindow->ScreenLogicalToPhysical(logicalPos);
+				m_SkinWindow->MoveWindow(physicalPos.x, physicalPos.y);
+
+				const int newY = m_SkinWindow->GetScreenLogicalPosition().y;
+				if (logicalPos.y != newY)
 				{
 					_itow_s(newY, buffer, 10);
 					Edit_SetText((HWND)lParam, buffer);

Library/Library.vcxproj
@@ -108,9 +108,6 @@
     <ClCompile Include="MeterString.cpp" />
     <ClCompile Include="Mouse.cpp" />
     <ClCompile Include="MonitorUtil.cpp" />
-    <ClCompile Include="MonitorUtil_Test.cpp">
-      <ExcludedFromBuild>$(ExcludeTests)</ExcludedFromBuild>
-    </ClCompile>
     <ClCompile Include="Net.cpp" />
     <ClCompile Include="NowPlaying\Cover.cpp" />
     <ClCompile Include="NowPlaying\Player.cpp" />

Library/Library.vcxproj.filters
@@ -146,7 +146,6 @@
     <ClCompile Include="MeterString.cpp" />
     <ClCompile Include="Mouse.cpp" />
     <ClCompile Include="MonitorUtil.cpp" />
-    <ClCompile Include="MonitorUtil_Test.cpp" />
     <ClCompile Include="Net.cpp" />
     <ClCompile Include="Rainmeter.cpp" />
     <ClCompile Include="Section.cpp" />

Library/MeasureMouse.cpp
@@ -362,7 +362,7 @@ void MeasureMouse::ReplaceMouseVariables(std::wstring& result, POINT screenPos) 
 	const auto pos =
 		m_RelativeToSkin ?
 		m_Skin->PhysicalToRelativeLogical(screenPos) :
-		MonitorUtil::GetMultiMonitorInfo().PhysicalToLogical(screenPos, (UINT)(m_Skin->GetDpiScale() * USER_DEFAULT_SCREEN_DPI));
+		m_Skin->PhysicalToScreenLogical(screenPos);
 	_itow_s(pos.x, mouseX, 10);
 	_itow_s(pos.y, mouseY, 10);
 

Library/MonitorUtil.cpp
@@ -76,8 +76,6 @@ void MultiMonitorInfo::Clear()
 	deviceCount = 0;
 	displayCount = 0;
 	monitors.clear();
-	horizontalSpans.clear();
-	verticalSpans.clear();
 }
 
 void MonitorUtil::InitializeMultiMonitorInfo()
@@ -214,8 +212,6 @@ void MonitorUtil::SetMultiMonitorInfo()
 		}
 	}
 
-	c_Monitors.UpdateSpans();
-
 	if (GetRainmeter().GetDebug())
 	{
 		LogDebug(L"------------------------------");
@@ -279,29 +275,6 @@ void MonitorUtil::UpdateWorkareaInfo()
 			}
 		}
 	}
-
-	c_Monitors.UpdateSpans();
-}
-
-void MultiMonitorInfo::UpdateSpans()
-{
-	horizontalSpans = CreateLogicalSpans(monitors, primary, true);
-	verticalSpans = CreateLogicalSpans(monitors, primary, false);
-
-	for (auto& monitor : monitors)
-	{
-		if (!monitor.active) continue;
-		if (monitor.logicalScreen.left != monitor.logicalScreen.right) continue;
-
-		const LONG left = ConvertPhysicalToLogical(monitor.screen.left, horizontalSpans);
-		const LONG top = ConvertPhysicalToLogical(monitor.screen.top, verticalSpans);
-		monitor.logicalScreen = {
-			left,
-			top,
-			left + MulDiv(monitor.screen.right - monitor.screen.left, USER_DEFAULT_SCREEN_DPI, monitor.dpi),
-			top + MulDiv(monitor.screen.bottom - monitor.screen.top, USER_DEFAULT_SCREEN_DPI, monitor.dpi)
-		};
-	}
 }
 
 const MonitorInfo* MultiMonitorInfo::GetForWindow(HWND window) const
@@ -328,188 +301,4 @@ const MonitorInfo* MultiMonitorInfo::GetByDisplayNumber(int screenNumber) const
 	}
 
 	return nullptr;
-}
-
-auto MultiMonitorInfo::CreateLogicalSpans(const std::vector<MonitorInfo>& monitors, int primary, bool horizontal) -> std::vector<Span>
-{
-	const auto& getAxisStart = [&](const MonitorInfo& monitor) -> LONG
-	{
-		return horizontal ? monitor.screen.left : monitor.screen.top;
-	};
-
-	const auto& getAxisEnd = [&](const MonitorInfo& monitor) -> LONG
-	{
-		return horizontal ? monitor.screen.right : monitor.screen.bottom;
-	};
-
-	std::vector<LONG> boundaries;
-	for (const auto& monitor : monitors)
-	{
-		if (!monitor.active) continue;
-
-		boundaries.push_back(getAxisStart(monitor));
-		boundaries.push_back(getAxisEnd(monitor));
-	}
-
-	std::sort(boundaries.begin(), boundaries.end());
-	boundaries.erase(std::unique(boundaries.begin(), boundaries.end()), boundaries.end());
-
-	std::vector<Span> spans;
-	if (boundaries.size() < 2) return spans;
-
-	// Split the virtual screen into non-overlapping axis spans so each span can be converted with
-	// the DPI of the monitor that covers the entire segment.
-	for (size_t i = 0; i + 1 < boundaries.size(); ++i)
-	{
-		const LONG start = boundaries[i];
-		const LONG end = boundaries[i + 1];
-		if (start == end) continue;
-
-		int monitorIndex = -1;
-		for (size_t j = 0; j < monitors.size(); ++j)
-		{
-			const auto& monitor = monitors[j];
-			if (monitor.active &&
-				getAxisStart(monitor) <= start &&
-				getAxisEnd(monitor) >= end)
-			{
-				if ((int)j + 1 == primary)
-				{
-					monitorIndex = (int)j;
-					break;
-				}
-
-				if (monitorIndex == -1)
-				{
-					monitorIndex = (int)j;
-				}
-			}
-		}
-
-		if (monitorIndex != -1)
-		{
-			spans.push_back({ start, end, 0, 0, monitors[monitorIndex].dpi });
-		}
-	}
-
-	if (spans.empty()) return spans;
-
-	// Keep the primary monitor's physical origin as the logical origin, then accumulate neighboring
-	// spans outward so mixed-DPI layouts stay continuous.
-	const LONG primaryStart =
-		primary > 0 && primary <= (int)monitors.size() ?
-		getAxisStart(monitors[primary - 1]) :
-		spans.front().physicalStart;
-
-	size_t anchor = 0;
-	for (size_t i = 0; i < spans.size(); ++i)
-	{
-		if (primaryStart >= spans[i].physicalStart && primaryStart < spans[i].physicalEnd)
-		{
-			anchor = i;
-			break;
-		}
-	}
-
-	spans[anchor].logicalStart =
-		primaryStart == spans[anchor].physicalStart ?
-		primaryStart :
-		primaryStart - MulDiv(primaryStart - spans[anchor].physicalStart, USER_DEFAULT_SCREEN_DPI, spans[anchor].dpi);
-	spans[anchor].logicalEnd =
-		spans[anchor].logicalStart + MulDiv(spans[anchor].physicalEnd - spans[anchor].physicalStart, USER_DEFAULT_SCREEN_DPI, spans[anchor].dpi);
-
-	for (size_t i = anchor + 1; i < spans.size(); ++i)
-	{
-		spans[i].logicalStart = spans[i - 1].logicalEnd;
-		spans[i].logicalEnd = spans[i].logicalStart + MulDiv(spans[i].physicalEnd - spans[i].physicalStart, USER_DEFAULT_SCREEN_DPI, spans[i].dpi);
-	}
-
-	for (size_t i = anchor; i > 0; --i)
-	{
-		spans[i - 1].logicalEnd = spans[i].logicalStart;
-		spans[i - 1].logicalStart = spans[i - 1].logicalEnd - MulDiv(spans[i - 1].physicalEnd - spans[i - 1].physicalStart, USER_DEFAULT_SCREEN_DPI, spans[i - 1].dpi);
-	}
-
-	return spans;
-}
-
-LONG MultiMonitorInfo::ConvertPhysicalToLogical(LONG value, const std::vector<Span>& spans, UINT dpiOverride)
-{
-	if (spans.empty()) return value;
-
-	for (const auto& span : spans)
-	{
-		if (value >= span.physicalStart && value <= span.physicalEnd)
-		{
-			const auto dpi = dpiOverride > 0 ? dpiOverride : span.dpi;
-			return span.logicalStart + MulDiv(value - span.physicalStart, USER_DEFAULT_SCREEN_DPI, dpi);
-		}
-	}
-
-	const auto& span = value < spans.front().physicalStart ? spans.front() : spans.back();
-	const auto dpi = dpiOverride > 0 ? dpiOverride : span.dpi;
-	return span.logicalStart + MulDiv(value - span.physicalStart, USER_DEFAULT_SCREEN_DPI, dpi);
-}
-
-LONG MultiMonitorInfo::ConvertLogicalToPhysical(LONG value, const std::vector<Span>& spans)
-{
-	if (spans.empty()) return value;
-
-	for (const auto& span : spans)
-	{
-		if (value >= span.logicalStart && value <= span.logicalEnd)
-		{
-			return span.physicalStart + MulDiv(value - span.logicalStart, span.dpi, USER_DEFAULT_SCREEN_DPI);
-		}
-	}
-
-	const auto& span = value < spans.front().logicalStart ? spans.front() : spans.back();
-	return span.physicalStart + MulDiv(value - span.logicalStart, span.dpi, USER_DEFAULT_SCREEN_DPI);
-}
-
-POINT MultiMonitorInfo::PhysicalToLogical(POINT point, UINT dpiOverride) const
-{
-	for (size_t i = 0; i < monitors.size(); ++i)
-	{
-		const auto& monitor = monitors[i];
-		if (monitor.active && Contains(monitor.screen, point))
-		{
-			const auto dpi = dpiOverride > 0 ? dpiOverride : monitor.dpi;
-			return {
-				monitor.logicalScreen.left + MulDiv(point.x - monitor.screen.left, USER_DEFAULT_SCREEN_DPI, dpi),
-				monitor.logicalScreen.top + MulDiv(point.y - monitor.screen.top, USER_DEFAULT_SCREEN_DPI, dpi)
-			};
-		}
-	}
-
-	// Points outside any monitor still convert against the nearest edge span so off-screen
-	// positions remain stable when skins are dragged past a boundary.
-	return {
-		ConvertPhysicalToLogical(point.x, horizontalSpans, dpiOverride),
-		ConvertPhysicalToLogical(point.y, verticalSpans, dpiOverride)
-	};
-}
-
-POINT MultiMonitorInfo::LogicalToPhysical(POINT point, UINT* dpi) const
-{
-	for (size_t i = 0; i < monitors.size(); ++i)
-	{
-		const auto& monitor = monitors[i];
-		if (monitor.active && Contains(monitor.logicalScreen, point))
-		{
-			if (dpi) *dpi = monitor.dpi;
-
-			return {
-				monitor.screen.left + MulDiv(point.x - monitor.logicalScreen.left, monitor.dpi, USER_DEFAULT_SCREEN_DPI),
-				monitor.screen.top + MulDiv(point.y - monitor.logicalScreen.top, monitor.dpi, USER_DEFAULT_SCREEN_DPI)
-			};
-		}
-	}
-
-	if (dpi) *dpi = 0;
-
-	return {
-		ConvertLogicalToPhysical(point.x, horizontalSpans),
-		ConvertLogicalToPhysical(point.y, verticalSpans)
-	};
 }

Library/MonitorUtil.h
@@ -44,28 +44,6 @@ struct MultiMonitorInfo
 	const MonitorInfo* GetByDeviceNumber(int deviceNumber) const;
 	const MonitorInfo* GetByDisplayNumber(int activeNumber) const;
 	const MonitorInfo* GetForWindow(HWND window) const;
-
-	void UpdateSpans();
-
-	POINT PhysicalToLogical(POINT point, UINT dpiOverride = 0) const;
-	POINT LogicalToPhysical(POINT point, UINT* dpi = nullptr) const;
-
-private:
-	struct Span
-	{
-		LONG physicalStart;
-		LONG physicalEnd;
-		LONG logicalStart;
-		LONG logicalEnd;
-		UINT dpi;
-	};
-
-	static LONG ConvertPhysicalToLogical(LONG value, const std::vector<Span>& spans, UINT dpiOverride = 0);
-	static LONG ConvertLogicalToPhysical(LONG value, const std::vector<Span>& spans);
-	static std::vector<Span> CreateLogicalSpans(const std::vector<MonitorInfo>& monitors, int primary, bool horizontal);
-
-	std::vector<Span> horizontalSpans;
-	std::vector<Span> verticalSpans;
 };
 
 namespace MonitorUtil {

commit 40df36c9377511f7f86f4c2b04ebd33daa916ce9

Skin: Fix scale flicker when dragging across DPI boundary
This was a regression in 2eae2d1.

Library/Skin.cpp
@@ -2061,7 +2061,17 @@ void Skin::ComputePositionFromOptions(bool inheritMonitorDpi)
 
 	const auto logicalPos = SkinPosition::ResolveScreenLogicalPosition(m_X, m_Y, m_WindowW, m_WindowH, m_ZoomScale, monitorsInfo);
 	UINT dpi = 0;
-	const auto physicalPos = System::ScreenLogicalToPhysical(logicalPos, &dpi);
+	POINT physicalPos;
+	if (m_State == STATE_RUNNING)
+	{
+		physicalPos = ScreenLogicalToPhysical(logicalPos);
+		dpi = GetDpiForWindow(m_Window);
+	}
+	else
+	{
+		physicalPos = System::ScreenLogicalToPhysical(logicalPos, &dpi);
+	}
+
 	m_X.pos = physicalPos.x;
 	m_Y.pos = physicalPos.y;
 
@@ -2069,6 +2079,7 @@ void Skin::ComputePositionFromOptions(bool inheritMonitorDpi)
 	{
 		dpi = System::GetSystemDpi();
 	}
+
 	if (inheritMonitorDpi)
 	{
 		UpdateWindowDpi(dpi);