9543de0c9138c2a3b24a9b074703742e6515ec64
Biru Mohanathas <birunthan@mohanathas.com>
2026-07-16 00:55:14 +0200

    MeasureRunCommand: Extract bits of RunCommand() into smaller functions for readability

--- Library/MeasureRunCommand.cpp
+++ Library/MeasureRunCommand.cpp
@@ -14,6 +14,7 @@
 #include "../Common/PathUtil.h"
 #include "../Common/StringUtil.h"
 #include <chrono>
+#include <optional>
 
 const WCHAR* err_UnknownCmd = L"Error 100: Unknown command: %s";
 const WCHAR* err_CmdRunning = L"Error 101: Program still running: %s";
@@ -22,6 +23,146 @@ const WCHAR* err_Process    = L"Error 103: Cannot start program: %s";
 const WCHAR* err_SaveFile   = L"Error 104: Cannot save file: %s";
 const WCHAR* err_Terminate  = L"Error 105: Cannot terminate process: %s";	// Rare!
 const WCHAR* err_CreatePipe = L"Error 106: Cannot create pipe";				// Rare!
+
+namespace {
+
+struct PipeHandles
+{
+	HANDLE outputReadTmp = INVALID_HANDLE_VALUE;
+	HANDLE inputWriteTmp = INVALID_HANDLE_VALUE;
+	HANDLE errorWrite = INVALID_HANDLE_VALUE;
+	HANDLE outputRead = INVALID_HANDLE_VALUE;
+	HANDLE inputWrite = INVALID_HANDLE_VALUE;
+};
+
+void CloseHandleIfValid(HANDLE& handle)
+{
+	if (handle != INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(handle);
+		handle = INVALID_HANDLE_VALUE;
+	}
+}
+
+void ClosePipeHandles(PipeHandles& pipes)
+{
+	CloseHandleIfValid(pipes.outputReadTmp);
+	CloseHandleIfValid(pipes.inputWriteTmp);
+	CloseHandleIfValid(pipes.errorWrite);
+	CloseHandleIfValid(pipes.inputWrite);
+	CloseHandleIfValid(pipes.outputRead);
+}
+
+std::optional<PipeHandles> CreateProcessPipes(STARTUPINFO& startupInfo)
+{
+	PipeHandles pipes;
+	SECURITY_ATTRIBUTES security = {};
+	security.nLength = sizeof(security);
+	security.bInheritHandle = TRUE;
+
+	const HANDLE currentProcess = GetCurrentProcess();
+
+	if (
+		CreatePipe(&pipes.outputReadTmp, &startupInfo.hStdOutput, &security, 0) &&
+		DuplicateHandle(currentProcess, startupInfo.hStdOutput, currentProcess, &pipes.errorWrite, 0, TRUE, DUPLICATE_SAME_ACCESS) &&
+		CreatePipe(&startupInfo.hStdInput, &pipes.inputWriteTmp, &security, 0) &&
+		DuplicateHandle(currentProcess, pipes.outputReadTmp, currentProcess, &pipes.outputRead, 0, FALSE, DUPLICATE_SAME_ACCESS) &&
+		DuplicateHandle(currentProcess, pipes.inputWriteTmp, currentProcess, &pipes.inputWrite, 0, FALSE, DUPLICATE_SAME_ACCESS))
+	{
+		startupInfo.hStdError = pipes.errorWrite;
+		return pipes;
+	}
+
+	ClosePipeHandles(pipes);
+	return std::nullopt;
+}
+
+void ReadAvailableOutput(HANDLE outputRead, OutputType type, std::wstring& result)
+{
+	auto appendOutputChunk = [&](const BYTE* buffer, DWORD bytesRead)
+	{
+		BYTE chunk[MAX_LINE_LENGTH + 3] = { 0 };
+		memcpy(chunk, buffer, bytesRead);
+
+		// Triple "null" the buffer in case an odd number of bytes is converted
+		// to a multi-byte string.
+		chunk[bytesRead] = '\0';
+		chunk[bytesRead + 1] = '\0';
+		chunk[bytesRead + 2] = '\0';
+
+		switch (type)
+		{
+		case OUTPUTTYPE_ANSI:
+			result += StringUtil::Widen(reinterpret_cast<LPCSTR>(chunk));
+			break;
+
+		case OUTPUTTYPE_UTF8:
+			result += StringUtil::WidenUTF8(reinterpret_cast<LPCSTR>(chunk));
+			break;
+
+		case OUTPUTTYPE_UTF16:
+		default:
+			result += reinterpret_cast<LPCWSTR>(chunk);
+			break;
+		}
+	};
+
+	BYTE peekBuffer[MAX_LINE_LENGTH + 3] = { 0 };
+	DWORD bytesAvailableNow = 0UL;
+	DWORD totalBytesAvailable = 0UL;
+	DWORD bytesLeftThisMessage = 0UL;
+
+	if (!PeekNamedPipe(outputRead, peekBuffer, MAX_LINE_LENGTH, &bytesAvailableNow, &totalBytesAvailable, &bytesLeftThisMessage) ||
+		bytesAvailableNow == 0)
+	{
+		return;
+	}
+
+	do
+	{
+		BYTE readBuffer[MAX_LINE_LENGTH + 3] = { 0 };
+		DWORD bytesRead = 0UL;
+		if (ReadFile(outputRead, readBuffer, MAX_LINE_LENGTH, &bytesRead, nullptr) == FALSE || bytesRead == 0)
+		{
+			break;
+		}
+
+		appendOutputChunk(readBuffer, bytesRead);
+		totalBytesAvailable = (totalBytesAvailable > bytesRead) ? (totalBytesAvailable - bytesRead) : 0;
+	}
+	while (totalBytesAvailable > 0);
+}
+
+bool SaveOutputToFile(const std::wstring& path, const std::wstring& output, OutputType type)
+{
+	std::wstring encoding = L"w+";
+	switch (type)
+	{
+	case OUTPUTTYPE_UTF8:
+		encoding.append(L", ccs=UTF-8");
+		break;
+
+	case OUTPUTTYPE_UTF16:
+		encoding.append(L", ccs=UTF-16LE");
+		break;
+
+	case OUTPUTTYPE_ANSI:
+	default:
+		break;
+	}
+
+	FILE* file = nullptr;
+	const bool opened = (_wfopen_s(&file, path.c_str(), encoding.c_str()) == 0) && file;
+	if (opened)
+	{
+		fputws(output.c_str(), file);
+		fclose(file);
+	}
+
+	return opened;
+}
+
+}  // namespace
 
 MeasureRunCommand::MeasureRunCommand(Skin* skin, const WCHAR* name) : Measure(skin, name),
 	m_Program(),
@@ -202,190 +343,93 @@ void MeasureRunCommand::RunCommand()
 	std::wstring folder = m_Folder;
 	WORD state = m_State;
 	int timeout = m_Timeout;
-	OutputType type = m_OutputType;
+	OutputType outputType = m_OutputType;
 
 	lock.unlock();
 
-	std::wstring result = L"";
+	std::wstring result;
 	bool error = false;
 
-	HANDLE read = INVALID_HANDLE_VALUE;
-	HANDLE write = INVALID_HANDLE_VALUE;
+	PROCESS_INFORMATION processInfo = {};
+	STARTUPINFO startupInfo = {};
+	startupInfo.cb = sizeof(startupInfo);
+	startupInfo.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+	startupInfo.wShowWindow = state;
 
-/*	Instead of trying to keep track of the following handles,
-	use an array to make cleanup easier.
-
-	HANDLE hOutputReadTmp;	0
-	HANDLE hOutputWrite;	1
-	HANDLE hInputWriteTmp;	2
-	HANDLE hInputRead;		3
-	HANDLE hErrorWrite;		4
-*/
-	HANDLE loadHandles[5] = { 0 };
-	for (int i = 0; i < sizeof(loadHandles) / sizeof(loadHandles[0]); ++i)
+	auto pipes = CreateProcessPipes(startupInfo);
+	if (pipes)
 	{
-		loadHandles[i] = INVALID_HANDLE_VALUE;
-	}
-
-	SECURITY_ATTRIBUTES sa = { 0 };
-	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
-	sa.bInheritHandle = TRUE;
-	sa.lpSecurityDescriptor = NULL;
-
-	HANDLE hProc = GetCurrentProcess();
-
-	// Create pipe for stdin, stdout, stderr
-	if (CreatePipe(&loadHandles[0], &loadHandles[1], &sa, 0) &&
-		DuplicateHandle(hProc, loadHandles[1], hProc, &loadHandles[4], 0, TRUE, DUPLICATE_SAME_ACCESS) &&
-		CreatePipe(&loadHandles[3], &loadHandles[2], &sa, 0) &&
-		DuplicateHandle(hProc, loadHandles[0], hProc, &read, 0, FALSE, DUPLICATE_SAME_ACCESS) &&
-		DuplicateHandle(hProc, loadHandles[2], hProc, &write, 0, FALSE, DUPLICATE_SAME_ACCESS))
-	{
-		BYTE buffer[MAX_LINE_LENGTH + 3] = { 0 };
-		DWORD bytesRead = 0UL;
-		DWORD totalBytes = 0UL;
-		DWORD bytesLeft = 0UL;
-		DWORD exit = 0UL;
-
-		PROCESS_INFORMATION pi = { 0 };
-		SecureZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
-
-		STARTUPINFO si = { 0 };
-		SecureZeroMemory(&si, sizeof(STARTUPINFO));
-		si.cb = sizeof(STARTUPINFO);
-		si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
-		si.wShowWindow = state;
-		si.hStdOutput  = loadHandles[1];
-		si.hStdInput   = loadHandles[3];
-		si.hStdError   = loadHandles[4];
-
-		if (CreateProcess(NULL, &command[0], NULL, NULL, TRUE, CREATE_NEW_PROCESS_GROUP, NULL, &folder[0], &si, &pi))
+		if (CreateProcess(nullptr, &command[0], nullptr, nullptr, TRUE, CREATE_NEW_PROCESS_GROUP, nullptr, &folder[0], &startupInfo, &processInfo))
 		{
-			// Store values inside measure for the "Close" or "Kill" command
 			lock.lock();
-				m_HProc = pi.hProcess;
-				m_DwPID = pi.dwProcessId;
+			m_HProc = processInfo.hProcess;
+			m_DwPID = processInfo.dwProcessId;
 			lock.unlock();
 
-			// Send command
 			DWORD written = 0UL;
-			WriteFile(write, &command[0], MAX_LINE_LENGTH, &written, nullptr);
+			WriteFile(pipes->inputWrite, &command[0], MAX_LINE_LENGTH, &written, nullptr);
 
-			std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-
-			// Read output of program (if any)
+			const std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
 			while (true)
 			{
-				auto ReadFileAndSetResult = [&]() -> void
+				DWORD exitCode = 0UL;
+
+				// Wait briefly before checking output so we do not busy-loop.
+				WaitForSingleObject(processInfo.hThread, 1);
+				ReadAvailableOutput(pipes->outputRead, outputType, result);
+
+				if (timeout >= 0 &&
+					std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count() > timeout)
 				{
-					if (ReadFile(read, buffer, MAX_LINE_LENGTH, &bytesRead, nullptr) != FALSE)
-					{
-						// Triple "null" the buffer in case an odd number bytes is
-						// converted to a multi-byte string.
-						buffer[bytesRead] = '\0';
-						buffer[bytesRead + 1] = '\0';
-						buffer[bytesRead + 2] = '\0';
-
-						switch (type)
-						{
-						case OUTPUTTYPE_ANSI:
-							result += StringUtil::Widen((LPCSTR)buffer);
-							break;
-
-						case OUTPUTTYPE_UTF8:
-							result += StringUtil::WidenUTF8((LPCSTR)buffer);
-							break;
-
-						default:
-						case OUTPUTTYPE_UTF16:
-							result += (LPCWSTR)buffer;
-							break;
-						}
-					}
-
-					SecureZeroMemory(buffer, sizeof(buffer));	// clear the buffer
-				};
-
-				// Wait for a signal from the process (or timeout).
-				// This reduced CPU usage significantly.
-				WaitForSingleObject(pi.hThread, 1);
-
-				// Check if there is any data to to read
-				PeekNamedPipe(read, buffer, MAX_LINE_LENGTH, &bytesRead, &totalBytes, &bytesLeft);
-				if (bytesRead != 0)
-				{
-					if (totalBytes > MAX_LINE_LENGTH)
-					{
-						while (bytesRead >= MAX_LINE_LENGTH)
-						{
-							ReadFileAndSetResult();
-						}
-					}
-					else
-					{
-						ReadFileAndSetResult();
-					}
-				}
-
-				// If a timeout is defined, attempt to terminate program and detach it from the measure
-				if ((timeout >= 0 && std::chrono::duration_cast<std::chrono::milliseconds>
-					(std::chrono::system_clock::now() - start).count() > timeout))
-				{
-					if (!TerminateApp(pi.hProcess, pi.dwProcessId, (state == SW_HIDE)))
+					if (!TerminateApp(processInfo.hProcess, processInfo.dwProcessId, (state == SW_HIDE)))
 					{
 						lock.lock();
-							m_Value = 105.0;
-							LogErrorF(this, err_Terminate, m_Program.c_str());	// Could not terminate process (very rare!)
-							error = true;
+						m_Value = 105.0;
+						LogErrorF(this, err_Terminate, m_Program.c_str());	// Could not terminate process (very rare!)
+						error = true;
 						lock.unlock();
 					}
+
 					break;
 				}
 
-				// Check to see if the program is still running
-				GetExitCodeProcess(pi.hProcess, &exit);
-				if (exit != STILL_ACTIVE) break;
+				GetExitCodeProcess(processInfo.hProcess, &exitCode);
+				if (exitCode != STILL_ACTIVE)
+				{
+					break;
+				}
 			}
 
-			// Close process handles
-			CloseHandle(pi.hThread);
-			CloseHandle(pi.hProcess);
+			CloseHandle(processInfo.hThread);
+			CloseHandle(processInfo.hProcess);
 
-			// Update values in case the "Close" or "Kill" command is called
 			lock.lock();
-				m_HProc = INVALID_HANDLE_VALUE;
-				m_DwPID = 0;
+			m_HProc = INVALID_HANDLE_VALUE;
+			m_DwPID = 0;
 			lock.unlock();
 		}
 		else
 		{
 			lock.lock();
-				m_Value = 103.0;
-				LogErrorF(this, err_Process, m_Program.c_str());	// Cannot start process
-				error = true;
+			m_Value = 103.0;
+			LogErrorF(this, err_Process, m_Program.c_str());	// Cannot start process
+			error = true;
 			lock.unlock();
 		}
 	}
 	else
 	{
 		lock.lock();
-			m_Value = 106.0;
-			LogErrorF(this, L"%s", err_CreatePipe);	// Cannot create pipe
-			error = true;
+		m_Value = 106.0;
+		LogErrorF(this, L"%s", err_CreatePipe);	// Cannot create pipe
+		error = true;
 		lock.unlock();
 	}
 
-	// Close handles
-	for (int i = 0; i < sizeof(loadHandles) / sizeof(loadHandles[0]); ++i)
+	if (pipes)
 	{
-		if (loadHandles[i] != INVALID_HANDLE_VALUE)
-		{
-			CloseHandle(loadHandles[i]);
-		}
+		ClosePipeHandles(*pipes);
 	}
-
-	CloseHandle(write);
-	CloseHandle(read);
 
 	// Remove any carriage returns
 	result.erase(std::remove(result.begin(), result.end(), L'\r'), result.end());
@@ -399,28 +443,11 @@ void MeasureRunCommand::RunCommand()
 
 		if (!m_OutputFile.empty())
 		{
-			std::wstring encoding = L"w+";
-			switch (type)
-			{
-			case OUTPUTTYPE_UTF8: { encoding.append(L", ccs=UTF-8"); break; }
-			case OUTPUTTYPE_UTF16: { encoding.append(L", ccs=UTF-16LE"); break; }
-			}
-
-			FILE* file = nullptr;
-			if ((_wfopen_s(&file, m_OutputFile.c_str(), encoding.c_str()) == 0) && file)
-			{
-				fputws(result.c_str(), file);
-			}
-			else
+			if (!SaveOutputToFile(m_OutputFile, result, outputType))
 			{
 				m_Value = 104.0;
 				LogErrorF(this, err_SaveFile, m_OutputFile.c_str());	// Cannot save file
 				error = true;
-			}
-
-			if (file)
-			{
-				fclose(file);
 			}
 		}
 
