7c8f8d988fd13739e6c6558522c9c3c798701923
Biru Mohanathas <birunthan@mohanathas.com>
2026-07-31 07:50:13 +0200

    Lua: Implement SKIN:SetVariable
    Examples:
    
    ```
    SKIN:SetVariable('name', 'value')
    SKIN:SetVariable('count', 42)
    SKIN:SetVariable({ name = 'value', count = 42 })
    ```

--- Library/lua/glue/LuaMeterWindow.cpp
+++ Library/lua/glue/LuaMeterWindow.cpp
@@ -128,6 +128,34 @@ static int GetVariable(lua_State* L)
 	return 1;
 }
 
+static int SetVariable(lua_State* L)
+{
+	DECLARE_SELF(L)
+	if (lua_istable(L, 2))
+	{
+		lua_pushnil(L);
+		while (lua_next(L, 2) != 0)
+		{
+			if (lua_type(L, -2) == LUA_TSTRING && lua_isstring(L, -1))
+			{
+				const std::wstring value = LuaHelper::ToWide(-1);
+				const std::wstring name = LuaHelper::ToWide(-2);
+				self->SetVariable(name, value);
+			}
+
+			lua_pop(L, 1);
+		}
+	}
+	else if (lua_isstring(L, 2) && lua_isstring(L, 3))
+	{
+		const std::wstring name = LuaHelper::ToWide(2);
+		const std::wstring value = LuaHelper::ToWide(3);
+		self->SetVariable(name, value);
+	}
+
+	return 0;
+}
+
 static int ReplaceVariables(lua_State* L)
 {
 	DECLARE_SELF(L)
@@ -228,6 +256,7 @@ void LuaScript::RegisterSkin(lua_State* L)
 		{ "SetOption", SetOption },
 		{ "SetOptionGroup", SetOptionGroup },
 		{ "GetVariable", GetVariable },
+		{ "SetVariable", SetVariable },
 		{ "ReplaceVariables", ReplaceVariables },
 		{ "ParseFormula", ParseFormula },
 		{ "MoveWindow", MoveWindow },
