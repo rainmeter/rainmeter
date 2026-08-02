36cd7bff01382cbd36b519bdaa9e2e2575e0d0a3
Biru Mohanathas <birunthan@mohanathas.com>
2026-08-02 17:24:33 +0200

    C++: Simplify license comment in remaining files

--- Library/MeasureAudio.cpp
+++ Library/MeasureAudio.cpp
@@ -1,9 +1,4 @@
-/* Copyright (C) 2026 Rainmeter Project Developers
- *
- * This Source Code Form is subject to the terms of the GNU General Public
- * License; either version 2 of the License, or (at your option) any later
- * version. If a copy of the GPL was not distributed with this file, You can
- * obtain one at <https://www.gnu.org/licenses/gpl-2.0.html>. */
+// Copyright (c) Rainmeter Team. Source code licensed under GNU GPL v2 (see LICENSE file).
 
 #include "StdAfx.h"
 #include "MeasureAudio.h"
