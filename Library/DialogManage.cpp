c466b4a2685d235406049927e8f8819720ac80ff
Biru Mohanathas <birunthan@mohanathas.com>
2026-09-11 20:04:02 -0700

    DialogManage: Add blue folder icon for skins tree
    Source/credits: https://www.deviantart.com/abs96/art/Windows-11-coloured-folder-icons-896431403

--- Library/DialogManage.cpp
+++ Library/DialogManage.cpp
@@ -594,10 +594,11 @@ void DialogManage::TabSkins::CreateImageList()
 	HWND tree = GetControl(Id_SkinsTreeView);
 	const int iconSize = MulDiv(16, (int)System::GetDpiForWindow(tree), USER_DEFAULT_SCREEN_DPI);
 	HIMAGELIST imageList = ImageList_Create(iconSize, iconSize, ILC_COLOR32, 2, 10);
-	HMODULE shell = GetModuleHandle(L"shell32");
 
-	HICON icon = (HICON)LoadImage(shell, MAKEINTRESOURCE(4), IMAGE_ICON, iconSize, iconSize, LR_SHARED);
+	HICON icon = GetIconBySize(IDI_FOLDER, iconSize);
 	ImageList_AddIcon(imageList, icon);
+
+	HMODULE shell = GetModuleHandle(L"shell32");
 	icon = (HICON)LoadImage(shell, MAKEINTRESOURCE(151), IMAGE_ICON, iconSize, iconSize, LR_SHARED);
 	ImageList_AddIcon(imageList, icon);
 
--- Library/resource.h
+++ Library/resource.h
@@ -2,6 +2,7 @@
 #define IDI_RAINMETER                                100
 //#define IDI_TRAY                                     101
 #define IDI_ADDFOLDER                                102
+#define IDI_FOLDER                                   103
 
 #define IDI_SKININSTALLER                            200
 #define IDB_INSTALLHEADER                            201
